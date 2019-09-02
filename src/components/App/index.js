93b57a1ef1af9bc877ed2bdd9e7ff30260fbe84e
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-02 16:21:42 +0000

    Rectify hello world

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,49 +1,46 @@
 import React from 'react';
-import './style.scss';
-
-// function App() {
-//   return (
-//     <div className='App'>
-//       <h1>Hello World</h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import * as antd from 'antd';
 
-const { Layout, Menu, Icon } = antd;
+import './style.scss';
 
+const { Layout, Menu, Icon } = antd;
 const { Header, Sider, Content } = Layout;
 
 export default class App extends React.Component {
-  state = {
-    collapsed: false,
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      collapsed: false,
+    };
+  }
 
   toggle = () => {
+    const { collapsed } = this.state;
+
     this.setState({
-      collapsed: !this.state.collapsed,
+      collapsed: !collapsed,
     });
   };
 
   render() {
+    const { collapsed } = this.state;
+
     return (
       <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className='logo' />
+          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+            <Menu.Item key='1'>
+              <Icon type='user' />
               <span>nav 1</span>
             </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
+            <Menu.Item key='2'>
+              <Icon type='video-camera' />
               <span>nav 2</span>
             </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
+            <Menu.Item key='3'>
+              <Icon type='upload' />
               <span>nav 3</span>
             </Menu.Item>
           </Menu>
@@ -51,8 +48,8 @@ export default class App extends React.Component {
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
             <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              className='trigger'
+              type={collapsed ? 'menu-unfold' : 'menu-fold'}
               onClick={this.toggle}
             />
           </Header>
