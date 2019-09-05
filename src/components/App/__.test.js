9e22eeee4ec098c056d5877c1f8977924075a699
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-05 15:07:49 +0000

    Refactor App component test

--- src/components/App/__.test.js
+++ src/components/App/__.test.js
@@ -8,4 +8,11 @@ describe('App component', () => {
   it('renders without crashing', () => {
     shallow(<App />);
   });
+
+  it('toggle side menu without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().sideMenuCollapsed).toEqual(false);
+    wrapper.instance().toggleSideMenu();
+    expect(wrapper.state().sideMenuCollapsed).toEqual(true);
+  });
 });
