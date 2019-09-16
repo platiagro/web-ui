b59553e32761abe9325b5938f9347cecc96a5d10
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-16 16:20:55 +0000

    Refactor MainContent to use react-router-dom

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import './style.scss';
 
 import { Layout } from 'antd';
 
 import ContentHeader from '../ContentHeader';
+
+import Projects from '../../pages/Projects';
+import E404 from '../../pages/E404';
 
 const { Content } = Layout;
 
@@ -12,7 +17,13 @@ const MainContent = () => (
   <div>
     <ContentHeader />
     <Content className='main-content'>
-      <div className='content' />
+      <div className='content'>
+        <Switch>
+          <Redirect exact from='/' to='/projects' />
+          <Route path='/projects' component={Projects} />
+          <Route component={E404} />
+        </Switch>
+      </div>
     </Content>
   </div>
 );
