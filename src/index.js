6d1ae0c13a9ef04128fc762c73d7f8f9b6ae0723
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-17 16:27:30 +0000

    Refactor root file (index.js) to support react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* istanbul ignore file */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
 import './style.scss';
@@ -8,7 +11,12 @@ import 'antd/dist/antd.css';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
