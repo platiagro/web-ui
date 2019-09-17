b9fc489e1b1afe05644dc999474218e2a203b5fd
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-17 17:57:40 +0000

    Refactor MainContent component to support routes

--- src/components/MainContent/.test.js
+++ src/components/MainContent/.test.js
@@ -4,13 +4,25 @@ import { shallow } from 'enzyme';
 
 import MainContent from '.';
 
+import mainRoutes from '../../routes/main';
+
 describe('MainContent component', () => {
   it('renders without crashing', () => {
-    shallow(<MainContent />);
+    const title = mainRoutes[0].title;
+    const subTitle = mainRoutes[0].subTitle;
+
+    shallow(
+      <MainContent title={title} subTitle={subTitle} mainRoutes={mainRoutes} />
+    );
   });
 
   it('renders html correctly', () => {
-    const mainContentShalowed = shallow(<MainContent />);
+    const title = mainRoutes[0].title;
+    const subTitle = mainRoutes[0].subTitle;
+
+    const mainContentShalowed = shallow(
+      <MainContent title={title} subTitle={subTitle} mainRoutes={mainRoutes} />
+    );
 
     expect(mainContentShalowed).toMatchSnapshot();
   });
