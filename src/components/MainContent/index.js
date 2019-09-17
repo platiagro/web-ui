cec25a27f886c4adfe6db7d76d50880f04c2e34d
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-09-17 13:24:27 +0000

    Refactor MainContent to use main routes

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './style.scss';
 
@@ -8,8 +8,7 @@ import { Layout } from 'antd';
 
 import ContentHeader from '../ContentHeader';
 
-import Projects from '../../pages/Projects';
-import E404 from '../../pages/E404';
+import mainRoutes from '../../routes/main';
 
 const { Content } = Layout;
 
@@ -19,9 +18,14 @@ const MainContent = () => (
     <Content className='main-content'>
       <div className='content'>
         <Switch>
-          <Redirect exact from='/' to='/projects' />
-          <Route path='/projects' component={Projects} />
-          <Route component={E404} />
+          {mainRoutes.map((route) => (
+            <Route
+              key={route.path}
+              exact={route.exact}
+              path={route.path}
+              component={route.component}
+            />
+          ))}
         </Switch>
       </div>
     </Content>
