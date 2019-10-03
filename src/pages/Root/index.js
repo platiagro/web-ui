13e19469d5099333e9a2c862f14f16a3d81f0626
Cedric Matheus <cedric.ms.alves@gmail.com>
2019-10-03 17:50:24 +0000

    Create Root page

---
+++ src/pages/Root/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Root = () => (
+  <div className='rootPage'>
+    <div className='rootPageBody'>
+      <div className='body'>Início</div>
+    </div>
+  </div>
+);
+
+export default Root;
