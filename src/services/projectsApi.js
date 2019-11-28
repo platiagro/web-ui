be47d7f8848d6884b1357002e1cdeef4954c47b2
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-11-28 14:42:34 -0300

    Add experimentComponent endpoints

--- src/services/projectsApi.js
+++ src/services/projectsApi.js
@@ -91,6 +91,86 @@ export const updateExperiment = async (projectId, experimentId, body) => {
   }
 };
 
+export const getExperimentComponents = async (projectId, experimentId) => {
+  try {
+    const response = await projectsApi.get(
+      `/projects/${projectId}/experiments/${experimentId}/components`
+    );
+    return response;
+  } catch (error) {
+    message.error(error.message);
+  }
+};
+
+export const getExperimentComponentById = async (
+  projectId,
+  experimentId,
+  componentId
+) => {
+  try {
+    const response = await projectsApi.get(
+      `/projects/${projectId}/experiments/${experimentId}/components/${componentId}`
+    );
+    return response;
+  } catch (error) {
+    message.error(error.message);
+  }
+};
+
+export const createExperimentComponent = async (
+  projectId,
+  experimentId,
+  componentId,
+  position
+) => {
+  try {
+    const body = {
+      componentId,
+      position,
+    };
+
+    const response = await projectsApi.post(
+      `/projects/${projectId}/experiments/${experimentId}/components`,
+      body
+    );
+    return response;
+  } catch (error) {
+    message.error(error.message);
+  }
+};
+
+export const updateExperimentComponent = async (
+  projectId,
+  experimentId,
+  componentId,
+  body
+) => {
+  try {
+    const response = await projectsApi.patch(
+      `/projects/${projectId}/experiments/${experimentId}/components/${componentId}`,
+      body
+    );
+    return response;
+  } catch (error) {
+    if (error.response) message.error(error.response.data.message);
+  }
+};
+
+export const deleteExperimentComponent = async (
+  projectId,
+  experimentId,
+  componentId
+) => {
+  try {
+    const response = await projectsApi.delete(
+      `/projects/${projectId}/experiments/${experimentId}/components/${componentId}`
+    );
+    return response;
+  } catch (error) {
+    message.error(error.message);
+  }
+};
+
 // \"name\": \"Auto-featuring Experiment\",
 // \"pipelineIdTrain\": \"23266cfd-4ed6-43d6-b8a0-ca8440d251c6\",
 // \"pipelineIdDeploy\": \"fe5205f5-7f76-4f57-84ca-ea6dd62670e8\",
