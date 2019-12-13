51c7a3e1a13724aea9eff9bd21f92df7d1146c2b
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2019-12-13 15:20:06 -0300

    Unit tests for services

--- src/services/resultsApi.js
+++ src/services/resultsApi.js
@@ -2,8 +2,10 @@
 import axios from 'axios';
 import { message } from 'antd';
 
+export const URL = process.env.REACT_APP_DATASET_API || '/datasets/apis';
+
 export const resultsApi = axios.create({
-  baseURL: process.env.REACT_APP_DATASET_API || '/datasets/apis',
+  baseURL: URL,
 });
 
 export const getResultTable = async (experimentId, task, headerId) => {
@@ -11,7 +13,6 @@ export const getResultTable = async (experimentId, task, headerId) => {
     const response = await resultsApi.get(
       `/results/${experimentId}/${task}/${headerId}`
     );
-
     return response.data.payload;
   } catch (error) {
     message.error(error.message);
@@ -23,7 +24,6 @@ export const getDatasetTable = async (experimentId, datasetId) => {
     const response = await resultsApi.get(
       `/results/${experimentId}/dataset/${datasetId}`
     );
-
     return response.data.payload;
   } catch (error) {
     message.error(error.message);
@@ -38,9 +38,7 @@ export const getPlot = async (experimentId) => {
       responseType: 'blob',
     });
     const imageUrl = urlCreator.createObjectURL(plot.data);
-
     const response = { type: type.data.type, imageUrl };
-
     return response;
   } catch (error) {
     message.error(error.message);
