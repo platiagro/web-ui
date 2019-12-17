9bea05cd7330adef8aaa8c954c4d3507457a67c2
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2019-12-17 11:37:05 -0300

    Unity tests

--- src/components/Component/NewParameterModal/__tests__/unity.test.js
+++ src/components/Component/NewParameterModal/__tests__/unity.test.js
@@ -4,41 +4,42 @@ import thunk from 'redux-thunk';
 import { Modal } from 'antd';
 import { shallow, mount } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
+import NewParameterModal from '..';
 
-import NewComponentModal from '.';
+const component = {
+  uuid: '1',
+  createdAt: '2019-11-05T17:16:53.000Z',
+  name: 'AutoML',
+  parameters: [
+    { name: 'b', type: 'int', required: true },
+    { name: 'a', type: 'int', required: false },
+  ],
+};
 
 const initialState = {
-  components: {
-    componentList: [],
-    modalIsVisible: false,
-    loading: false,
-    error: null,
+  component: {
+    details: component,
   },
 };
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const store = mockStore(initialState);
+const onCancel = () => {};
 
-jest.mock('react-router-dom', () => ({
-  useHistory: () => ({
-    push: jest.fn(),
-  }),
-}));
-
-describe('NewComponentModal component', () => {
+describe('NewParameterModal component', () => {
   it('is expected render without crashing', () => {
     shallow(
       <Provider store={store}>
-        <NewComponentModal />
+        <NewParameterModal visible onCancel={onCancel} />
       </Provider>
     );
   });
 
   it('is expected render html correctly', () => {
-    const wrapper = mount(
+    const wrapper = shallow(
       <Provider store={store}>
-        <NewComponentModal />
+        <NewParameterModal visible onCancel={onCancel} />
       </Provider>
     );
     expect(wrapper).toMatchSnapshot();
@@ -47,28 +48,38 @@ describe('NewComponentModal component', () => {
   it('should render self and subcomponents', () => {
     const wrapper = mount(
       <Provider store={store}>
-        <NewComponentModal />
+        <NewParameterModal visible onCancel={onCancel} />
       </Provider>
     );
 
-    expect(wrapper.find('Form(NewComponentModal)').exists()).toBeTruthy();
-    expect(wrapper.find(NewComponentModal).exists()).toBeTruthy();
+    expect(wrapper.find('Form(NewParameterModal)').exists()).toBeTruthy();
+    expect(wrapper.find(NewParameterModal).exists()).toBeTruthy();
     expect(wrapper.find(Modal).exists()).toBeTruthy();
   });
 
   it('onOk should be called', () => {
     const wrapper = mount(
       <Provider store={store}>
-        <NewComponentModal />
+        <NewParameterModal visible onCancel={onCancel} />
       </Provider>
     );
 
     const event = { preventDefault: () => {} };
     const modal = wrapper.find(Modal).instance();
     const spyHandleOk = jest.spyOn(modal, 'handleOk');
-    const spyHandleCancel = jest.spyOn(modal, 'handleCancel');
     modal.handleOk(event);
     expect(spyHandleOk).toHaveBeenCalled();
+  });
+
+  it('onCancel should be called', () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <NewParameterModal visible onCancel={onCancel} />
+      </Provider>
+    );
+
+    const modal = wrapper.find(Modal).instance();
+    const spyHandleCancel = jest.spyOn(modal, 'handleCancel');
     modal.handleCancel();
     expect(spyHandleCancel).toHaveBeenCalled();
   });
