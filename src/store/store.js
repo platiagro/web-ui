8632393f7005ccd4ffc55a2b22dec124d9b80be4
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2020-01-10 17:25:41 +0000

    Add trace to Redux DevTools

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })) ||
+  compose;
 
 const store = createStore(
   rootReducer,
