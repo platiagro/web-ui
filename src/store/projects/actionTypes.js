cbb038a68959b50fbbffc98065e9381c1bad4232
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-03-11 16:33:07 +0000

    Projects store action types created

---
+++ src/store/projects/actionTypes.js
@@ -0,0 +1,18 @@
+/**
+ * projects action types
+ */
+// fetch project
+const FETCH_PROJECTS = 'FETCH_PROJECTS';
+
+// create project
+const CREATE_PROJECT = 'CREATE_PROJECT';
+
+// open project
+const OPEN_PROJECT = 'OPEN_PROJECT';
+
+// EXPORT
+export default {
+  FETCH_PROJECTS,
+  CREATE_PROJECT,
+  OPEN_PROJECT,
+};
