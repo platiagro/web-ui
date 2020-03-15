c8006e7ba1e8be5577960b168791865de1a7888f
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-03-15 22:39:30 +0000

    ImplantedExperiments store actions created

---
+++ src/store/implantedExperiments/actions.js
@@ -0,0 +1,43 @@
+// ACTION TYPES
+import actionTypes from './actionTypes';
+
+// MOCKS
+// implanted experiments mock
+import implantedExperimentsMock from '../../components/Content/ImplantedExperimentsContent/_/_implantedExperimentsMock';
+
+// ACTIONS
+/**
+ * fetch implanted experiments action
+ * @returns {type, implantedExperiments}
+ */
+export const fetchImplantedExperiments = () => ({
+  type: actionTypes.FETCH_IMPLANTED_EXPERIMENTS,
+  implantedExperiments: implantedExperimentsMock,
+});
+
+/**
+ * test implanted experiment inference action
+ * @param {string} implantedExperimentUuid
+ * @param {Object} file
+ * @returns {type, inferenceResult}
+ */
+export const testImplantedExperimentInference = (
+  implantedExperimentUuid,
+  file
+) => ({
+  type: actionTypes.TEST_IMPLANTED_EXPERIMENT_INFERENCE,
+  inferenceResult: [0.9, 0.1],
+});
+
+/**
+ * delete implanted experiment action
+ * @param {string} implantedExperimentUuid
+ * @returns {type, implantedExperiments}
+ */
+export const deleteImplantedExperiment = (implantedExperimentUuid) => ({
+  type: actionTypes.DELETE_IMPLANTED_EXPERIMENT,
+  implantedExperiments: implantedExperimentsMock.filter(
+    (implantedExperiment) =>
+      implantedExperiment.uuid !== implantedExperimentUuid
+  ),
+});
