8dff032f4924d4e24f7b228794c23ea7405f96a2
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-03-16 13:22:15 +0000

    ImplantedExperiments store reducer created

--- src/store/implantedExperiments/reducer.js
+++ src/store/implantedExperiments/reducer.js
@@ -11,8 +11,6 @@ const implantedExperiments = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_IMPLANTED_EXPERIMENTS:
       return action.project;
-    case actionTypes.TEST_IMPLANTED_EXPERIMENT_INFERENCE:
-      return action.project;
     case actionTypes.DELETE_IMPLANTED_EXPERIMENT:
       return action.project;
     default:
