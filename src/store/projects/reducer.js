1ec241c8ae0e751a8407f2f00cdb47d23516197b
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-03-18 15:54:27 +0000

    Projects store reducer refactored

--- src/store/projects/reducer.js
+++ src/store/projects/reducer.js
@@ -3,6 +3,7 @@ import { message } from 'antd';
 
 // ACTION TYPES
 import actionTypes from './actionTypes';
+import projectActionTypes from '../project/actionTypes';
 
 // INITIAL STATE
 const initialState = [];
@@ -18,6 +19,9 @@ const projects = (state = initialState, action) => {
     // fetch projects fail
     case actionTypes.FETCH_PROJECTS_FAIL:
       return message.error(action.errorMessage);
+    // fetch projects success
+    case projectActionTypes.DELETE_PROJECT_SUCCESS:
+      return state.filter((project) => project.uuid !== action.projectId);
     default:
       return state;
   }
