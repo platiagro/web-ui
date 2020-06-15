cd753c295e79ed03a7a3e800b564b11528fc7211
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-06-15 17:31:27 -0300

    Fix JSON and styles

--- src/components/Content/ImplantedExperimentsContent/UsingDeploymentsModal/Container.js
+++ src/components/Content/ImplantedExperimentsContent/UsingDeploymentsModal/Container.js
@@ -10,47 +10,41 @@ const ContentInfo = () => {
     border: '1px solid #c1c7d0',
     borderRadius: '3px',
     background: '#f4f5f7',
+    padding: '10px',
   };
 
-  const ndarray = `{"ndarray":[...]}`;
-  const body = `{
-    "names": {
-        ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
-    }
-    "data": {
-      "ndarray": [
-        [5.1, 3.5, 1.4, 0.2],
-        [6.3, 2.3, 4.4, 1.3]
+  const request = JSON.stringify({
+    'data': {
+      'names': ['atributo1', 'atributo2'],
+      'ndarray': [
+        [0., 10.],
       ]
-  }`;
-
-  const requestInfo = `
-    curl -X POST \ -H 'Content-Type: application/json' \ -d '{"data": { "ndarray": [[5.1,3.5,1.4,0.2]], "names":["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"]}}' \
-    http://localhost/seldon/deployments/ff885bf3-c587-4e2a-aad8-1ac394426796/api/v1.0/predictions
-  `;
-
-  const response = `
-  {
-    "meta": { "puid": "pqvaab0ej28n89sr4ffjni1ie7", 
-    "tags": { },
-      "routing": { },
-      "requestPath": {
-        "e6065e85-a056-40b7-9e4b-4db49ee3b915": "platiagro/platiagro-deployment-image:0.0.2"
-      },
-      "metrics": []
-    },
-    "data": {
-      "names": ["Iris-setosa", "Iris-versicolor", "Iris-virginica"],
-      "ndarray": [[0.8902377788100774, 0.10971507514730343, 4.714604261906825E-5]]
     }
-  }
-  `;
+  }, null, 2);
+
+  const response = JSON.stringify({
+    'meta': {
+      'puid': 'pqvaab0ej28n89sr4ffjni1ie7',
+      'tags': {
+      },
+      'routing': {
+      },
+      'requestPath': {
+        'e6065e85-a056-40b7-9e4b-4db49ee3b915': 'platiagro/platiagro-deployment-image:0.0.2'
+      },
+      'metrics': []
+    },
+    'data': {
+      'names': ['atributo1', 'atributo2', 'proba_classe1', 'proba_classe2'],
+      'ndarray': [[0., 10., 0.8902377788100774, 0.10971507514730343]]
+    }
+  }, null, 2);
 
   return (
     <div>
       <p>
         As aplicações clientes devem enviar uma requisição HTTP (REST) para a
-        seguinte API.
+        seguinte API:
       </p>
       <pre>
         POST /seldon/deployments/id-do-experimento/api/v1.0/predictions
@@ -58,24 +52,25 @@ const ContentInfo = () => {
         Headers: Content-Type: application/json
       </pre>
 
-      <p>RequestBody:</p>
-      <pre style={styles}> {body} </pre>
+      <h3>Request Body:</h3>
+      <pre style={styles}>{request}</pre>
       <p></p>
       <p>
-        "names":[...] - nome das colunas do conjunto de dados, sem o nome da
-        variável alvo. "data":{ndarray} - amostras para predição, sem a variável
-        alvo.
+        <b>"names":[...] </b>- nome das colunas do conjunto de dados, sem o nome do
+        atributo alvo.
+        <br />
+        <b>"ndarray":[...]</b> - amostras para predição, sem o valor atributo alvo.
       </p>
 
-      <p>Por exemplo, a seguinte requisição poderia ser realizada:</p>
-
-      <p>
-        <pre style={styles}>{requestInfo}</pre>
-      </p>
-
-      <p>Essa requisição retorna uma resposta similar a seguinte:</p>
+      <h3>Response Body:</h3>
       <p>
         <pre style={styles}>{response}</pre>
+      </p>
+      <p></p>
+      <p>
+        <b>"names":[...] </b>- nome das colunas retornadas pelo último passo do fluxo de experimentos.
+        <br />
+        <b>"ndarray":[...]</b> - valores retornados pelo último passo do fluxo de experimentos.
       </p>
     </div>
   );
