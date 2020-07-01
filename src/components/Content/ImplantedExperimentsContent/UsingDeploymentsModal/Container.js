0d5e0a0961d406aba2eb29e4dd1773b7f159b58f
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-07-01 10:26:21 -0300

    Bump to 0.1.0

--- src/components/Content/ImplantedExperimentsContent/UsingDeploymentsModal/Container.js
+++ src/components/Content/ImplantedExperimentsContent/UsingDeploymentsModal/Container.js
@@ -32,7 +32,7 @@ const ContentInfo = () => {
         routing: {},
         requestPath: {
           'e6065e85-a056-40b7-9e4b-4db49ee3b915':
-            'platiagro/platiagro-deployment-image:0.0.2',
+            'platiagro/platiagro-deployment-image:0.1.0',
         },
         metrics: [],
       },
