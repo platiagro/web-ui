4a26da3a6781755157554380d48211388277bdd2
Ricardo Shiguemi Hiramatsu <38075762+shiguemi32@users.noreply.github.com>
2020-08-03 16:14:55 -0300

    Fix train and deploy experiments (#134)
    Co-authored-by: Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>

--- src/store/pipelines/actions.js
+++ src/store/pipelines/actions.js
@@ -78,7 +78,7 @@ export const trainExperimentRequest = (experiment, operators) => (
   const trainObject = { experimentId };
 
   // getting operators
-  trainObject.components = operators.map((operator) => {
+  trainObject.operators = operators.map((operator) => {
     // configuring parameters
     const configuredParameters = operator.parameters.map((parameter) => ({
       name: parameter.name,
@@ -257,7 +257,7 @@ export const deployExperimentRequest = (
   };
 
   // getting operators
-  deployObject.components = operators.map((operator) => ({
+  deployObject.operators = operators.map((operator) => ({
     commands: operator.commands,
     dependencies: operator.dependencies,
     notebookPath: operator.deploymentNotebookPath,
