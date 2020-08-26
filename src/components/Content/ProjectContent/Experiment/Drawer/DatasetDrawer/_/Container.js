815c7d9978b040e02c5d458639434e0b7d5365f3
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-08-26 15:37:31 +0000

    Removing Columns Table from Dataset Drawer

--- src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/Container.js
+++ src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/Container.js
@@ -5,63 +5,16 @@ import { connect } from 'react-redux';
 // COMPONENTS
 import DatasetDrawer from './index';
 
-// ACTIONS
-import { updateDatasetColumnRequest } from '../../../../../../../store/dataset/actions';
-
-// DISPATCHS
-const mapDispatchToProps = (dispatch) => {
-  return {
-    // update dataset column
-    handleUpdateDatasetColumn: (datasetName, columnName, columnNewType) =>
-      dispatch(
-        updateDatasetColumnRequest(datasetName, columnName, columnNewType)
-      ),
-  };
-};
-
-// STATES
-const mapStateToProps = (state) => {
-  return {
-    dataset: state.datasetReducer,
-    loading: state.uiReducer.datasetOperator.loading,
-    trainingLoading: state.uiReducer.experimentTraining.loading,
-  };
-};
-
 /**
  * DatasetDrawer Container.
  * This component is responsible for create a logic container for drawer with
  * redux.
  *
- * @param props
  */
-const DatasetDrawerContainer = (props) => {
-  // destructuring props
-  const {
-    dataset,
-    loading,
-    trainingLoading,
-    handleUpdateDatasetColumn,
-  } = props;
-
-  // HANDLERS
-  // update dataset column
-  const updateDatasetColumnHandler = (columnName, columnNewValue) =>
-    handleUpdateDatasetColumn(columnName, columnNewValue);
-
+const DatasetDrawerContainer = () => {
   // RENDER
-  return (
-    <DatasetDrawer
-      columns={dataset.columns}
-      handleSetColumnType={updateDatasetColumnHandler}
-      loading={loading}
-      trainingLoading={trainingLoading}
-    />
-  );
+  return <DatasetDrawer />;
 };
 
 // EXPORT
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DatasetDrawerContainer);
+export default connect(null, null)(DatasetDrawerContainer);
--- src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/index.js
+++ src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/index.js
@@ -1,80 +1,23 @@
 // CORE LIBS
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
 // CONTAINERS
 import { DatasetUploadInputBlockContainer } from 'containers';
-
-// COMPONENTS
-import ColumnsTable from '../ColumnsTable/_';
-import { InputBlockContainer } from 'components';
 
 /**
  * Dataset Drawer.
  * This component is responsible for displaying dataset content in drawer.
  *
- * @param props
  */
-const DatasetDrawer = (props) => {
-  const { loading, trainingLoading, columns, handleSetColumnType } = props;
-  // CONSTANTS
-  // show dataset columns
-  const showColumns = columns !== undefined && columns.length > 0;
-
-  const [currentPage, setCurrentPage] = useState(1);
-
-  // handler to set dataset column type
-  const handleChangeColumnType = (e, row) => {
-    // get header id, column id and column position on array
-    const { name: columnId } = row;
-    // set column type const
-    const columnType = e;
-    // setting column type
-    handleSetColumnType(columnId, columnType);
-  };
-
-  // render dataset columns table
-  const renderColumnsTable = () => {
-    // rendering table
-    return columns.length === 0 ? null : (
-      // div container
-      <InputBlockContainer title='Tipos de atributos'>
-        {/* dataset columns table */}
-        <ColumnsTable
-          columns={columns}
-          handleChangeType={handleChangeColumnType}
-          disabled={trainingLoading}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-          loading={loading}
-        />
-      </InputBlockContainer>
-    );
-  };
-
+const DatasetDrawer = () => {
   // RENDER
   return (
     // div container
     <div>
       {/* dataset upload input block container */}
       <DatasetUploadInputBlockContainer />
-
-      {/* columns table */}
-      {showColumns && renderColumnsTable()}
     </div>
   );
-};
-
-// PROP TYPES
-DatasetDrawer.propTypes = {
-  /** dataset drawer is loading dataset */
-  loading: PropTypes.bool.isRequired,
-  /** dataset drawer dataset columns */
-  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
-  /** dataset drawer set column type handler */
-  handleSetColumnType: PropTypes.func.isRequired,
-  /** experiment is training */
-  trainingLoading: PropTypes.bool.isRequired,
 };
 
 // EXPORT
