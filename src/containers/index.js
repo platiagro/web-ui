87a242891d49c59c7427443db440e8c914366448
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-08-26 16:38:17 +0000

    Creating Data View Button Container

--- src/containers/index.js
+++ src/containers/index.js
@@ -7,8 +7,12 @@ import DatasetUploadInputBlockContainer from './DatasetUploadInputBlockContainer
 // OPERATOR RESULTS MODAL MODAL CONTAINER
 import OperatorResultsModalContainer from './OperatorResultsModalContainer';
 
+// DATA VIEW BUTTON CONTAINER
+import DataViewButtonContainer from './DataViewButtonContainer';
+
 export {
   OperatorResizableSectionContainer,
   DatasetUploadInputBlockContainer,
   OperatorResultsModalContainer,
+  DataViewButtonContainer,
 };
