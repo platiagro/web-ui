4f413d749f695bd681b82370aecd9cc25b106916
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-08-26 16:40:01 +0000

    Adding data view button container to drawer

--- src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/Container.js
+++ src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/Container.js
@@ -5,16 +5,27 @@ import { connect } from 'react-redux';
 // COMPONENTS
 import DatasetDrawer from './index';
 
+// STATES
+const mapStateToProps = (state) => {
+  return {
+    datasetColumns: state.datasetReducer.columns,
+  };
+};
+
 /**
  * DatasetDrawer Container.
  * This component is responsible for create a logic container for drawer with
  * redux.
  *
+ * @param props
  */
-const DatasetDrawerContainer = () => {
+const DatasetDrawerContainer = (props) => {
+  // destructuring props
+  const { datasetColumns } = props;
+
   // RENDER
-  return <DatasetDrawer />;
+  return <DatasetDrawer columns={datasetColumns} />;
 };
 
 // EXPORT
-export default connect(null, null)(DatasetDrawerContainer);
+export default connect(mapStateToProps, null)(DatasetDrawerContainer);
--- src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/index.js
+++ src/components/Content/ProjectContent/Experiment/Drawer/DatasetDrawer/_/index.js
@@ -1,23 +1,59 @@
 // CORE LIBS
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // CONTAINERS
-import { DatasetUploadInputBlockContainer } from 'containers';
+import {
+  DatasetUploadInputBlockContainer,
+  DataViewButtonContainer,
+} from 'containers';
+
+// COMPONENTS
+import { InputBlockContainer } from 'components';
 
 /**
  * Dataset Drawer.
  * This component is responsible for displaying dataset content in drawer.
  *
+ * @param props
  */
-const DatasetDrawer = () => {
-  // RENDER
+const DatasetDrawer = (props) => {
+  // PROPS / CONSTANTS
+  // destructuring props
+  const { columns } = props;
+
+  // show data view button
+  const showDataViewButton = columns !== undefined && columns.length > 0;
+
+  // RENDERS
+  // render data view button
+  const renderDataViewButton = () => {
+    // rendering table
+    return columns.length === 0 ? null : (
+      // input container
+      <InputBlockContainer>
+        <DataViewButtonContainer />
+      </InputBlockContainer>
+    );
+  };
+
+  // render component
   return (
     // div container
     <div>
       {/* dataset upload input block container */}
       <DatasetUploadInputBlockContainer />
+
+      {/* data view button */}
+      {showDataViewButton && renderDataViewButton()}
     </div>
   );
+};
+
+// PROP TYPES
+DatasetDrawer.propTypes = {
+  /** dataset drawer dataset columns */
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 // EXPORT
