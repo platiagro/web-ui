7fe99cf628f48ac3820fc3bf30c80e1faa7890d9
Schaffer <schaffer@cpqd.com.br>
2020-09-02 19:09:32 -0300

    Add skeleton on taskboxes loading

---
+++ src/components/Content/ProjectContent/Experiment/ExperimentFlow/LoadingBox/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//UI Libs
+import { Skeleton } from 'antd';
+import {
+  LoadingOutlined,
+} from '@ant-design/icons';
+
+// STYLES
+import './style.less';
+
+const LoadingBox = () =>
+  <div className='card skeleton-task-box'>
+    <div className='siders'>
+      <div style={{ fontSize: '18px' }}><LoadingOutlined spin /></div>
+    </div>
+    <div className='middle' style={{ padding: 12 }}>
+      <Skeleton
+        paragraph={{ rows: 2 }}
+        size='large'
+        title={false}
+      />
+    </div>
+    <div className='siders'></div>
+  </div>
+
+// PROP TYPES
+LoadingBox.propTypes = {
+  /** string with classes */
+  className: PropTypes.string,
+};
+
+export default LoadingBox;
