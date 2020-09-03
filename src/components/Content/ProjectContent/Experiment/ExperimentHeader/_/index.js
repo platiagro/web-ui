dd674a19873b19233fca1986178dbe133cb56177
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-09-03 15:24:46 -0300

    Removes delete experiment button (#179)
    This button in not necessary, since we can delete experiments using
    tabs.

--- src/components/Content/ProjectContent/Experiment/ExperimentHeader/_/index.js
+++ src/components/Content/ProjectContent/Experiment/ExperimentHeader/_/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // COMPONENTS
-import DeleteExperimentButton from '../DeleteExperimentButton';
 import DeployExperimentButton from '../DeployExperimentButton';
 import InterruptTrainExperimentButton from '../InterruptTrainExperimentButton';
 import NewTemplateButton from '../NewTemplateButton/Container';
@@ -25,54 +24,45 @@ const ExperimentHeader = ({
   trainingLoading,
   trainingSucceeded,
   deployStatus,
-  handleDeleteExperiment,
   handleTrainExperiment,
   handleDeployExperiment,
   handleDeleteTrainExperiment,
   empty,
 }) => (
-  <div className='buttons-config'>
-    <NewTemplateModal />
-    {/* new template button */}
-    <NewTemplateButton disabled={loading || trainingLoading || empty} />
-    {/* train button or interrupt train button */}
-    {trainingLoading ? (
-      <InterruptTrainExperimentButton
-        handleClick={handleDeleteTrainExperiment}
-        disabled={loading || deleteTrainingLoading}
-        deleteExperimentRunning={deleteTrainingLoading}
+    <div className='buttons-config'>
+      <NewTemplateModal />
+      {/* new template button */}
+      <NewTemplateButton disabled={loading || trainingLoading || empty} />
+      {/* train button or interrupt train button */}
+      {trainingLoading ? (
+        <InterruptTrainExperimentButton
+          handleClick={handleDeleteTrainExperiment}
+          disabled={loading || deleteTrainingLoading}
+          deleteExperimentRunning={deleteTrainingLoading}
+        />
+      ) : (
+          <TrainExperimentButton
+            handleClick={handleTrainExperiment}
+            disabled={loading || empty}
+          />
+        )}
+      {/* deploy button */}
+      <DeployExperimentButton
+        handleClick={handleDeployExperiment}
+        disabled={
+          loading ||
+          trainingLoading ||
+          !trainingSucceeded ||
+          deployStatus === 'Succeeded' ||
+          deployStatus === 'Running'
+        }
+        loading={deployStatus === 'Running'}
       />
-    ) : (
-      <TrainExperimentButton
-        handleClick={handleTrainExperiment}
-        disabled={loading || empty}
-      />
-    )}
-    {/* deploy button */}
-    <DeployExperimentButton
-      handleClick={handleDeployExperiment}
-      disabled={
-        loading ||
-        trainingLoading ||
-        !trainingSucceeded ||
-        deployStatus === 'Succeeded' ||
-        deployStatus === 'Running'
-      }
-      loading={deployStatus === 'Running'}
-    />
-    {/* delete button */}
-    <DeleteExperimentButton
-      disabled={loading || trainingLoading}
-      handleClick={handleDeleteExperiment}
-      loading={loading}
-    />
-  </div>
-);
+    </div>
+  );
 
 // PROP TYPES
 ExperimentHeader.propTypes = {
-  /** experiment header delete experiment handler */
-  handleDeleteExperiment: PropTypes.func.isRequired,
   /** experiment header edit experiment name handler */
   handleEditExperimentName: PropTypes.func.isRequired,
   /** experiment header train experiment handler */
