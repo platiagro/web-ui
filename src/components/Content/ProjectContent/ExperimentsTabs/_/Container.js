d23318722c4f6a75ebb9b331c7d4804ff4e05a9e
samborba <bf_samuel@hotmail.com>
2020-09-14 15:36:18 -0300

    Fix Side Menu Content - whenever there was an change between experiments, the side menu should return to the initial state

--- src/components/Content/ProjectContent/ExperimentsTabs/_/Container.js
+++ src/components/Content/ProjectContent/ExperimentsTabs/_/Container.js
@@ -18,6 +18,8 @@ import {
   editExperimentNameRequest,
 } from '../../../../../store/experiment/actions';
 
+import { deselectOperator } from '../../../../../store/operator/actions';
+
 // DISPATCHS
 const mapDispatchToProps = (dispatch, routerProps) => {
   return {
@@ -46,6 +48,7 @@ const mapDispatchToProps = (dispatch, routerProps) => {
           newPosition
         )
       ),
+    handleDeselectOperator: () => dispatch(deselectOperator()),
   };
 };
 
@@ -77,6 +80,7 @@ const ExperimentTabsContainer = (props) => {
     handleClearAllExperiments,
     handleDeleteExperiment,
     handleRenameExperiment,
+    handleDeselectOperator,
   } = props;
 
   // CONSTANTS
@@ -126,6 +130,7 @@ const ExperimentTabsContainer = (props) => {
       // routing
       history.push(`/projetos/${projectId}/${targetId}`);
     }
+    handleDeselectOperator();
   };
   // organizing tabs
   const handleOrganizeTabs = (dragExperimentId, hoverExperimentId) => {
