3ffa4347fe2b6590a7ca13f9bf6e2dcf1360c455
Hugo Oliveira <65179840+hugooliv4@users.noreply.github.com>
2020-09-17 16:23:19 -0300

    Toolbar on experiments tab

---
+++ src/components/Content/ProjectContent/Experiment/ExperimentHeader/ToolbarConfig/index.js
@@ -0,0 +1,52 @@
+// CORE LIBS
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// ANTD LIBS
+import {
+  RedoOutlined,
+  UndoOutlined,
+  DeleteOutlined,
+  ZoomInOutlined,
+  ZoomOutOutlined,
+} from '@ant-design/icons';
+
+import { Button, Popconfirm } from 'antd';
+
+const ToolbarConfig = ({ handleDeleteClick, disabled, loading, operator }) => {
+  return (
+    <>
+      <Button icon={<UndoOutlined />} type='text' disabled />
+      <Button icon={<RedoOutlined />} type='text' disabled />
+      <Button icon={<ZoomInOutlined />} type='text' disabled />
+      <Button icon={<ZoomOutOutlined />} type='text' disabled />
+      <Popconfirm
+        title='Você tem certeza que deseja excluir esta tarefa?'
+        onConfirm={handleDeleteClick}
+        okText='Sim'
+        cancelText='Não'
+        disabled={!operator?.uuid}
+      >
+        <Button
+          icon={<DeleteOutlined />}
+          type='text'
+          onClick={(e) => e.stopPropagation()}
+          disabled={!operator?.uuid}
+        />
+      </Popconfirm>
+    </>
+  );
+};
+
+// PROP TYPES
+ToolbarConfig.propTypes = {
+  /** remove operator button is disabled */
+  disabled: PropTypes.bool.isRequired,
+  /** remove operator button click function */
+  handleClick: PropTypes.func.isRequired,
+  /** is loading */
+  loading: PropTypes.bool.isRequired,
+};
+
+// EXPORT
+export default ToolbarConfig;
--- src/components/Content/ProjectContent/Experiment/ExperimentHeader/_/index.js
+++ src/components/Content/ProjectContent/Experiment/ExperimentHeader/_/index.js
@@ -7,6 +7,7 @@ import InterruptTrainExperimentButton from '../InterruptTrainExperimentButton';
 import NewTemplateButton from '../NewTemplateButton/Container';
 import NewTemplateModal from '../NewTemplateModal/Container';
 import TrainExperimentButton from '../TrainExperimentButton';
+import ToolbarConfig from '../ToolbarConfig';
 
 import './styles.less';
 
@@ -15,6 +16,45 @@ import './styles.less';
  * This component is responsible for displaying the experiment header.
  *
  * @component
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.loading
+ * @param props.deleteTrainingLoading
+ * @param props.trainingLoading
+ * @param props.handleTrainExperiment
+ * @param props.handleDeleteTrainExperiment
+ * @param props.empty
+ * @param props.handleRemoveOperatorClick
+ * @param props.handleRemoveOperator
+ * @param props.operator
  * @param {object} props Component props
  * @returns {ExperimentHeader} React component
  */
@@ -24,25 +64,35 @@ const ExperimentHeader = ({
   trainingLoading,
   handleTrainExperiment,
   handleDeleteTrainExperiment,
+  handleRemoveOperator,
   empty,
+  operator,
 }) => (
   <div className='buttons-config'>
-    <NewTemplateModal />
-    {/* new template button */}
-    <NewTemplateButton disabled={loading || trainingLoading || empty} />
-    {/* train button or interrupt train button */}
-    {trainingLoading ? (
-      <InterruptTrainExperimentButton
-        handleClick={handleDeleteTrainExperiment}
-        disabled={loading || deleteTrainingLoading}
-        deleteExperimentRunning={deleteTrainingLoading}
+    <div>
+      <ToolbarConfig
+        handleDeleteClick={handleRemoveOperator}
+        operator={operator}
       />
-    ) : (
-      <TrainExperimentButton
-        handleClick={handleTrainExperiment}
-        disabled={loading || empty}
-      />
-    )}
+    </div>
+    <div>
+      <NewTemplateModal />
+      {/* new template button */}
+      <NewTemplateButton disabled={loading || trainingLoading || empty} />
+      {/* train button or interrupt train button */}
+      {trainingLoading ? (
+        <InterruptTrainExperimentButton
+          handleClick={handleDeleteTrainExperiment}
+          disabled={loading || deleteTrainingLoading}
+          deleteExperimentRunning={deleteTrainingLoading}
+        />
+      ) : (
+        <TrainExperimentButton
+          handleClick={handleTrainExperiment}
+          disabled={loading || empty}
+        />
+      )}
+    </div>
   </div>
 );
 
