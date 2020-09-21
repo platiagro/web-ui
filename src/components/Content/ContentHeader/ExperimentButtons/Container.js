3da58bd920962601efe2b3f92e9073d7de6e78a2
samborba <bf_samuel@hotmail.com>
2020-09-21 19:40:34 -0300

    Requested Changes - improved operator verification - add new experiment status validation method

--- src/components/Content/ContentHeader/ExperimentButtons/Container.js
+++ src/components/Content/ContentHeader/ExperimentButtons/Container.js
@@ -49,14 +49,17 @@ const ExperimentButtonsContainer = ({
 }) => {
   // CONSTANTS
   const { experimentId } = useParams();
-  const { deployStatus, succeeded: trainingSucceeded } = experiment;
+  const { deployStatus } = experiment;
 
   // Checks if the experiment has, at least, one non DATASET operator
-  const hasExecutorOperator = operators
-    .map((operator) => {
-      return operator.tags.includes('DATASETS') && operator.tags.length > 0;
-    })
-    .every((value) => value === true);
+  const hasExecutorOperator = operators.some((operator) => {
+    return !operator.tags.includes('DATASETS') && operator.tags.length > 0;
+  });
+
+  // Check if any operator has failed
+  const hasFailed = operators.some((operator) => {
+    return operator.status === 'Failed';
+  });
 
   // HOOKS
   // did mount hook
@@ -77,10 +80,10 @@ const ExperimentButtonsContainer = ({
     <ExperimentButtons
       handleClick={deployExperimentHandler}
       disabled={
-        hasExecutorOperator ||
+        !hasExecutorOperator ||
         loading ||
         trainingLoading ||
-        !trainingSucceeded ||
+        hasFailed ||
         deployStatus === 'Succeeded' ||
         deployStatus === 'Running'
       }
