3285a3530a8906ec22eb5586e0c14bc4bb004a86
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-10-19 17:54:59 +0000

    Add refresh and close prompt

--- src/containers/ChangeRoutePromptContainer/ChangeRoutePromptContainer.jsx
+++ src/containers/ChangeRoutePromptContainer/ChangeRoutePromptContainer.jsx
@@ -1,5 +1,5 @@
 // REACT LIBS
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -13,6 +13,12 @@ const mapStateToProps = (state) => {
   };
 };
 
+/**
+ * Change Route Prompt Container
+ *
+ * @param {object} props Component props
+ * @returns {ChangeRoutePromptContainer} React + Redux Container
+ */
 const ChangeRoutePromptContainer = (props) => {
   // PROPS / CONSTANTS
   // destructuring props
@@ -21,6 +27,19 @@ const ChangeRoutePromptContainer = (props) => {
   // prompt message
   const message =
     'Você está enviando um arquivo, ao sair da tela atual o envio será cancelado automaticamente. Deseja continuar?';
+
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      if (datasetIsUploading) e.returnValue = message;
+    };
+
+    window.onbeforeunload = handleBeforeUnload;
+
+    // unmount
+    return () => {
+      window.onbeforeunload = null;
+    };
+  }, [message, datasetIsUploading]);
 
   // rendering component
   return <Prompt when={datasetIsUploading} message={message} />;
