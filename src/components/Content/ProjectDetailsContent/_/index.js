16ed19d94df309c2d745b9caa1adb43833c76f80
Hugo Oliveira <65179840+hugooliv4@users.noreply.github.com>
2020-11-09 11:36:27 -0300

    Modifying the left menu

--- src/components/Content/ProjectDetailsContent/_/index.js
+++ src/components/Content/ProjectDetailsContent/_/index.js
@@ -14,35 +14,28 @@ import ProjectDetailContainer from 'containers/ProjectDetailsContainer/index';
 const { Sider, Content } = Layout;
 
 const ProjectsDetailsContent = () => {
-   
   const renderFlowContent = () => (
-   
-
-    <Layout style={{ overflow: 'hidden'}}>
-      <Sider width={250}    
-       className={'detailsMenu'} id="menudetails"
-      >
-      <TasksMenuDetailsContainer/>
+    <Layout style={{ overflow: 'hidden' }}>
+      <Sider width={'20%'}>
+        <TasksMenuDetailsContainer />
       </Sider>
       <Content>
         <Layout style={{ height: '100%' }} className={'content'}>
-         <ProjectDetailContainer/>
-          
+          <ProjectDetailContainer />
         </Layout>
       </Content>
     </Layout>
   );
 
-  return(
-   // fragment
-   <>
-    <DndProvider backend={HTML5Backend}>
-    <HeaderProjectDetailsContainer />
-    {renderFlowContent()}
-    </DndProvider>
-   
- </>
-  )
-}
+  return (
+    // fragment
+    <>
+      <DndProvider backend={HTML5Backend}>
+        <HeaderProjectDetailsContainer />
+        {renderFlowContent()}
+      </DndProvider>
+    </>
+  );
+};
 
 export default ProjectsDetailsContent;
