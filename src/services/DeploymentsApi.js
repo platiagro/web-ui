44b121762ca3964547eaa4853a2add4ebdb28e6d
Schaffer <mr.giancarloschaffer@gmail.com>
2020-11-24 14:51:31 -0300

    Create services for CRUD deployments

--- src/services/DeploymentsApi.js
+++ src/services/DeploymentsApi.js
@@ -32,10 +32,35 @@ const testDeployedExperiments = (id, body) => {
   return seldonApi.post(`/deployments/${id}/api/v1.0/predictions`, body);
 };
 
+const listDeployments = (projectsId) => {
+  return pipelinesApi.get(`${projectsId}/${deploymentsPath}`);
+};
+
+const createDeployment = (projectsId, body) => {
+  return pipelinesApi.post(`${projectsId}/${deploymentsPath}`, body);
+};
+
+const updateDeployment = (projectsId, deploymentId, body) => {
+  return pipelinesApi.patch(
+    `${projectsId}/${deploymentsPath}/${deploymentId}`,
+    body
+  );
+};
+
+const deleteDeployment = (projectsId, deploymentId) => {
+  return pipelinesApi.delete(
+    `${projectsId}/${deploymentsPath}/${deploymentId}`
+  );
+};
+
 // EXPORT DEFAULT
 export default {
   fetchDeployedExperiment,
   fetchDeployedExperimentLogs,
   testDeployedExperiments,
   deleteDeployedExperiments,
+  listDeployments,
+  createDeployment,
+  updateDeployment,
+  deleteDeployment,
 };
