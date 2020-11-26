6a2e5ed93efaf19b1ba0679d91ad2e3a93b41c55
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-11-26 14:12:02 +0000

    Add styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sections: [
+    // Módulos de componentes do projeto
+    {
+      name: 'Components',
+      description: 'Componentes da aplicação.',
+      components: ['src/components/**/*.component.jsx'],
+    },
+  ],
+};
