6290497619a0286d4333249b97c5b5c892c30c4a
Schaffer Jr <30741185+schafferjrdev@users.noreply.github.com>
2020-11-27 08:31:26 -0300

    Create route 'pre-implantacao' with placeholder (#287)
    * Create route 'pre-implantacao' with placeholder
    
    * Use placeholder on 'pre-implantacao' route

--- src/components/Content/_/index.js
+++ src/components/Content/_/index.js
@@ -41,6 +41,9 @@ const Content = () => {
         <Route exact path='/projetos/:projectId/experimentos/:experimentId?'>
           <ExperimentsContent />
         </Route>
+        <Route exact path='/projetos/:projectId/pre-implantacao/:deploymentId?'>
+          <span>Pré-implantação placeholder</span>
+        </Route>
         {/* task content */}
         <Route exact path='/tarefas'>
           <TaskContent />
