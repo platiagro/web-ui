01172c6814b574d93e6b0f969c841443642a88d2
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-12-01 18:52:58 +0000

    Use routes module

--- src/components/Content/_/index.js
+++ src/components/Content/_/index.js
@@ -5,15 +5,10 @@ import { Switch, Route } from 'react-router-dom';
 // UI LIBS
 import { Layout } from 'antd';
 
-// CONTENTS
-import Error404Content from '../Error404Content';
-import ExperimentsContent from '../ExperimentsContent/_';
-import ProjectsContent from '../ProjectsContent/_';
-import ProjectsDetailsContent from '../ProjectDetailsContent/_';
-import TaskContent from '../TasksContent/_';
-
 // STYLES
 import './style.less';
+
+import Routes from 'Routes';
 
 /**
  * Content.
@@ -25,33 +20,15 @@ const Content = () => {
     // layout component
     <Layout>
       <Switch>
-        {/* projects content */}
-        <Route exact path='/'>
-          <ProjectsContent />
-        </Route>
-        {/* projects content */}
-        <Route exact path='/projetos'>
-          <ProjectsContent />
-        </Route>
-        {/* project content */}
-        <Route exact strict path='/projetos/:projectId'>
-          <ProjectsDetailsContent />
-        </Route>
-        {/* experiment content */}
-        <Route exact path='/projetos/:projectId/experimentos/:experimentId?'>
-          <ExperimentsContent />
-        </Route>
-        <Route exact path='/projetos/:projectId/pre-implantacao/:deploymentId?'>
-          <span>Pré-implantação placeholder</span>
-        </Route>
-        {/* task content */}
-        <Route exact path='/tarefas'>
-          <TaskContent />
-        </Route>
-        {/* error 404 content */}
-        <Route path='*'>
-          <Error404Content />
-        </Route>
+        {Routes.map((route) => {
+          const { exact, path, strict } = route;
+
+          return (
+            <Route key={path} exact={exact} path={path} strict={strict}>
+              <route.component />
+            </Route>
+          );
+        })}
       </Switch>
     </Layout>
   );
