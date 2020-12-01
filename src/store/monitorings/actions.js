46336128ac642997587fc1c086a936ad7bf2c749
Alende Geraldo <meubebe21@hotmail.com>
2020-12-01 13:04:33 +0000

    Ajustes na action criando metodos de success para todas as requisicoes e adicionando blocos try catch e arquico reducer

--- src/store/monitorings/actions.js
+++ src/store/monitorings/actions.js
@@ -5,24 +5,32 @@ import monitoringApi from 'services/MonitoringsApi';
 // UI LIB
 import { message } from 'antd';
 
-export const getMonitoring = (projectId) => (dispatch) => {
+export const getMonitoring = (projectId) => async (dispatch, getState) => {
+  const { monitorings } = getState();
   dispatch({
     type: actionTypes.GET_MONITORING_REQUEST,
   });
-  monitoringApi
-    .requestMonitoring(projectId)
-    .then((response) => {
-      if (response) {
-        dispatch({
-          type: actionTypes.GET_MONITORING_SUCCESS,
-        });
-        return response.data;
-      }
-      return;
-    })
-    .catch((error) => {
-      dispatch(getMonitoringFail(error));
-    });
+  try {
+    const response = await monitoringApi.requestMonitoring(projectId);
+    if (response) {
+      dispatch(getMonitoringSuccess(monitorings, response));
+    }
+  } catch (error) {
+    dispatch(getMonitoringFail(error));
+  }
+};
+
+/**
+ *
+ * @param {object} monitorings
+ * @param {object} response
+ */
+const getMonitoringSuccess = (monitorings, response) => (dispatch) => {
+  // dispatching get monitoring success
+  dispatch({
+    type: actionTypes.CREATE_MONITORING_SUCCESS,
+    monitorings: [...monitorings, response.data],
+  });
 };
 
 /**
@@ -42,26 +50,38 @@ const getMonitoringFail = (error) => (dispatch) => {
   message.error(errorMessage);
 };
 
-export const createMonitoring = (projectId, body) => (dispatch) => {
+export const createMonitoring = (projectId, body) => async (
+  dispatch,
+  getState
+) => {
   dispatch({
     type: actionTypes.CREATE_MONITORING_REQUEST,
   });
-  monitoringApi
-    .poststMonitoring(projectId, body)
-    .then((response) => {
-      if (response) {
-        dispatch({
-          type: actionTypes.CREATE_MONITORING_SUCCESS,
-        });
-        return response.data;
-      }
-      return;
-    })
-    .catch((error) => {
-      dispatch(createMonitoringFail(error));
-    });
+  const { monitorings } = getState();
+  try {
+    const response = await monitoringApi.poststMonitoring(projectId, body);
+    if (response) {
+      dispatch(createMonitoringSuccess(monitorings, response));
+    }
+  } catch (error) {
+    dispatch(createMonitoringFail(error));
+  }
 };
 
+/**
+ * get monitoring
+ *
+ * @param monitorings
+ * @param response
+ *
+ */
+const createMonitoringSuccess = (monitorings, response) => (dispatch) => {
+  // dispatching get monitoring success
+  dispatch({
+    type: actionTypes.CREATE_MONITORING_SUCCESS,
+    monitorings: [...monitorings, response.data],
+  });
+};
 /**
  * get monitoring
  *
@@ -79,24 +99,37 @@ const createMonitoringFail = (error) => (dispatch) => {
   message.error(errorMessage);
 };
 
-export const updateMonitoring = (projectId, body) => (dispatch) => {
+export const updateMonitoring = (projectId, body) => async (
+  dispatch,
+  getState
+) => {
+  const { counter } = getState();
+
   dispatch({
     type: actionTypes.UPDATE_MONITORING_REQUEST,
   });
-  monitoringApi
-    .updateMonitoring(projectId, body)
-    .then((response) => {
-      if (response) {
-        dispatch({
-          type: actionTypes.UPDATE_MONITORING_SUCCESS,
-        });
-        return response.data;
-      }
-      return;
-    })
-    .catch((error) => {
-      dispatch(updateMonitoringFail(error));
-    });
+  try {
+    const response = await monitoringApi.updateMonitoring(projectId, body);
+    if (response) {
+      counter.dispatch(updateMonitoringSuccess(response));
+    }
+  } catch (error) {
+    dispatch(updateMonitoringFail(error));
+  }
+};
+
+/**
+ * get monitoring
+ *
+ * @param response
+ *
+ */
+const updateMonitoringSuccess = (response) => (dispatch) => {
+  // dispatching get monitoring success
+  dispatch({
+    type: actionTypes.UPDATE_MONITORING_SUCCESS,
+    monitoringItem: response.data,
+  });
 };
 
 /**
@@ -116,24 +149,33 @@ const updateMonitoringFail = (error) => (dispatch) => {
   message.error(errorMessage);
 };
 
-export const deleteMonitoring = (projectId) => (dispatch) => {
+export const deleteMonitoring = (projectId) => async (dispatch, getState) => {
+  const { counter } = getState();
+
   dispatch({
     type: actionTypes.DELETE_MONITORING_REQUEST,
   });
-  monitoringApi
-    .deleteMonitoring(projectId)
-    .then((response) => {
-      if (response) {
-        dispatch({
-          type: actionTypes.DELETE_MONITORING_SUCCESS,
-        });
-        return response.data;
-      }
-      return;
-    })
-    .catch((error) => {
-      dispatch(deleteMonitoringFail(error));
-    });
+  try {
+    const response = await monitoringApi.deleteMonitoring(projectId);
+    if (response) {
+      counter.dispatch(deleteMonitoringSucess(response));
+    }
+  } catch (error) {
+    dispatch(deleteMonitoringFail(error));
+  }
+};
+
+/**
+ * get monitoring success
+ *
+ * @param {object} response
+ */
+const deleteMonitoringSucess = (response) => (dispatch) => {
+  // dispatching get monitoring success
+  dispatch({
+    type: actionTypes.DELETE_MONITORING_SUCCESS,
+    monitoringItem: response.data,
+  });
 };
 
 /**
--- src/store/monitorings/monitoringReducer.js
+++ src/store/monitorings/monitoringReducer.js
@@ -1,26 +1,13 @@
 import actionTypes from './actionTypes';
 
-const initialState = {
-  monitorings: [],
-};
+const initialState = [];
 
 const monitoringReducer = (state = initialState, action = undefined) => {
   switch (action.type) {
     case actionTypes.CREATE_MONITORING_SUCCESS:
-      return {
-        ...state,
-        monitorings: [...state.monitorings, action.GET_MONITORING_REQUEST],
-      };
     case actionTypes.DELETE_MONITORING_SUCCESS:
-      return {
-        ...state,
-        monitorings: [...state.monitorings, action.GET_MONITORING_REQUEST],
-      };
     case actionTypes.UPDATE_MONITORING_SUCCESS:
-      return {
-        ...state,
-        monitorings: [...state.monitorings, action.GET_MONITORING_REQUEST],
-      };
+      return [...action.monitorings];
     default:
       return state;
   }
