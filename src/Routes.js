39b3ac2373f03867a61d3aa3af4031052e4a5b8c
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-12-01 18:11:49 +0000

    Add pre-implantacao route

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,12 @@
+// FIXME: Remover quando componente (deployment) estiver pronto
+import React from 'react';
+
 import ProjectsContent from 'components/Content/ProjectsContent/_';
 import ProjectsDetailsContent from 'components/Content/ProjectDetailsContent/_';
 import ExperimentsContent from 'components/Content/ExperimentsContent/_';
+
+// FIXME: Remover quando componente estiver pronto
+const tempDeploymentComponent = () => <span>Pré-implantação placeholder</span>;
 
 const Routes = [
   {
@@ -23,6 +29,11 @@ const Routes = [
     path: '/projetos/:projectId/experimentos/:experimentId?',
     exact: true,
     component: ExperimentsContent,
+  },
+  {
+    path: '/projetos/:projectId/pre-implantacao/:deploymentId?',
+    exact: true,
+    component: tempDeploymentComponent,
   },
 ];
 
