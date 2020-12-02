6ec75d6839a93ca27fa32e1f07c90bb6810b775e
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-12-02 12:51:03 -0300

    Feature - Create Routes Module (#294)
    * Create routes module
    
    * Add root route
    
    * Add projetos route
    
    * Add detalhe projeto route
    
    * Add experimento route
    
    * Add pre-implantacao route
    
    * Add tarefas route
    
    * Add error404 route
    
    * Move routes module
    
    * Create route typedef
    
    * Create routes typedef
    
    * Add routes type
    
    * Create routes module index
    
    * Add routes alias
    
    * Use routes module

---
+++ src/Routes/Routes.js
@@ -0,0 +1,54 @@
+// FIXME: Remover quando componente (deployment) estiver pronto
+import React from 'react';
+
+import ProjectsContent from 'components/Content/ProjectsContent/_';
+import ProjectsDetailsContent from 'components/Content/ProjectDetailsContent/_';
+import ExperimentsContent from 'components/Content/ExperimentsContent/_';
+import TaskContent from 'components/Content/TasksContent/_';
+import Error404Content from 'components/Content/Error404Content';
+
+// FIXME: Remover quando componente estiver pronto
+const tempDeploymentComponent = () => <span>Pré-implantação placeholder</span>;
+
+/**
+ * @type {Routes}
+ */
+const Routes = [
+  {
+    path: '/',
+    exact: true,
+    component: ProjectsContent,
+  },
+  {
+    path: '/projetos',
+    exact: true,
+    component: ProjectsContent,
+  },
+  {
+    path: '/projetos/:projectId',
+    exact: true,
+    strict: true,
+    component: ProjectsDetailsContent,
+  },
+  {
+    path: '/projetos/:projectId/experimentos/:experimentId?',
+    exact: true,
+    component: ExperimentsContent,
+  },
+  {
+    path: '/projetos/:projectId/pre-implantacao/:deploymentId?',
+    exact: true,
+    component: tempDeploymentComponent,
+  },
+  {
+    path: '/tarefas',
+    exact: true,
+    component: TaskContent,
+  },
+  {
+    path: '*',
+    component: Error404Content,
+  },
+];
+
+export default Routes;
