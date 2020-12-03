66be76d5f480b58b6bdddff214e241dd7aebec5f
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-12-03 13:30:42 +0000

    Create Deployment Page structure

---
+++ src/Pages/Deployment/Deployment.page.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * Pagina de pré-implantação/implantação.
+ */
+function Deployment() {
+  // FIXME: Adicionar containers
+  return (
+    <div>
+      <div>menu</div>
+      <div>titulo</div>
+      <div>opcoes</div>
+      <div>fluxo</div>
+      <div>monitoramento</div>
+      <div>experimentos</div>
+      <div>parametros</div>
+    </div>
+  );
+}
+
+export default Deployment;
