7bc775591dd7136d449c352bc4f9d94332e5433d
Cedric Matheus <cedric.ms.alves@gmail.com>
2020-12-03 15:39:51 +0000

    Create Deployment Page styles

--- src/Pages/Deployment/Deployment.page.jsx
+++ src/Pages/Deployment/Deployment.page.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
+import './Deployment.style.less';
+
 /**
  * Pagina de pré-implantação/implantação.
  */
 function Deployment() {
   // FIXME: Adicionar containers
+  // FIXME: Criar Layout
   return (
-    <div>
-      <div>menu</div>
-      <div>titulo</div>
-      <div>opcoes</div>
-      <div>fluxo</div>
-      <div>monitoramento</div>
-      <div>experimentos</div>
-      <div>parametros</div>
+    <div className='deploymentPage'>
+      <div className='menu'>menu</div>
+      <div className='contentBlock'>
+        <div className='title'>titulo</div>
+        <div className='deploymentContent'>
+          <div className='flowContent'>
+            <div className='options'>opcoes</div>
+            <div className='flow'>fluxo</div>
+            <div className='monitoring'>monitoramento</div>
+            <div className='parameters'>parametros</div>
+          </div>
+          <div className='experiments'>experimentos</div>
+        </div>
+      </div>
     </div>
   );
 }
