f0d114969e05472b8991645abe6e93d714da1393
Hugo Oliveira <65179840+hugooliv4@users.noreply.github.com>
2021-02-01 11:17:21 -0300

    ExternalDatasetHelperModal

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -1,9 +1,11 @@
 import InferenceTestResultModal from './InferenceTestResultModal';
 import NewMonitoringModal from './NewMonitoringModal';
 import UsingDeploymentsModal from './UsingDeploymentsModal';
+import ExternalDatasetHelperModal from './ExternalDatasetHelperModal';
 
 export {
   InferenceTestResultModal,
   NewMonitoringModal,
   UsingDeploymentsModal,
+  ExternalDatasetHelperModal,
 };
