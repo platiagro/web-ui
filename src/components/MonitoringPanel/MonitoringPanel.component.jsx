3c13ca2c9a996836f7ca1b6770dac07f5b7098a3
Luan Eduardo da Costa <luan1346@gmail.com>
2021-03-25 09:55:13 -0300

    Fix skeleton styles

--- src/components/MonitoringPanel/MonitoringPanel.component.jsx
+++ src/components/MonitoringPanel/MonitoringPanel.component.jsx
@@ -23,11 +23,11 @@ const MonitoringPanel = ({
   return (
     <div className={`monitoring-panel ${className}`}>
       {isLoading ? (
-        <>
+        <div className="monitoring-panel-skeletons">
           <Skeleton.Button className="monitoring-panel-skeleton" size="large" active />
           <Skeleton.Button className="monitoring-panel-skeleton" size="large" active />
           <Skeleton.Button className="monitoring-panel-skeleton" size="large" active />
-        </>
+        </div>
       ) : (
         <>
           {!!monitorings && monitorings.length > 0 ? (
