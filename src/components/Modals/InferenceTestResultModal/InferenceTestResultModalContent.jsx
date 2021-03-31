35174ff8c914033e2bf29072a1972222289d66a5
Schaffer <mr.giancarloschaffer@gmail.com>
2021-03-31 01:50:53 -0300

    Refactor nested ternary

--- src/components/Modals/InferenceTestResultModal/InferenceTestResultModalContent.jsx
+++ src/components/Modals/InferenceTestResultModal/InferenceTestResultModalContent.jsx
@@ -23,6 +23,20 @@ const { TextArea } = Input;
 const InferenceTestResultModalContent = (props) => {
   const { experimentInference, getDeployExperimentLogs, retryTest } = props;
 
+  const showMedia = () =>
+    utils.isImage(experimentInference) ? (
+      <img
+        src={utils.formatBase64(experimentInference)}
+        alt='predict-response'
+        className='image-difference'
+        style={{ maxWidth: '100%' }}
+      />
+    ) : (
+      <video src={experimentInference.binData} controls>
+        <track default kind='captions' />
+      </video>
+    );
+
   /**
    * Function to return either image, video or text
    *
@@ -31,18 +45,7 @@ const InferenceTestResultModalContent = (props) => {
   const adaptableComponent = () => (
     <div className='container-difference'>
       {utils.isSupportedBinaryData(experimentInference) ? (
-        utils.isImage(experimentInference) ? (
-          <img
-            src={utils.formatBase64(experimentInference)}
-            alt='predict-response'
-            className='image-difference'
-            style={{ maxWidth: '100%' }}
-          />
-        ) : (
-          <video src={experimentInference.binData} controls>
-            <track default kind='captions' />
-          </video>
-        )
+        showMedia()
       ) : (
         <div className='iterative-prediction'>
           <h3>Resposta do Modelo</h3>
@@ -57,6 +60,62 @@ const InferenceTestResultModalContent = (props) => {
     </div>
   );
 
+  const renderModalBody = () => (
+    <>
+      {'ndarray' in experimentInference ? (
+        <CommonTable
+          columns={experimentInference.names.map((name) => ({
+            title: name,
+            dataIndex: name,
+            key: name,
+            width: 100,
+          }))}
+          dataSource={experimentInference.ndarray.map((e, i) => {
+            const data = { key: i };
+            experimentInference.names.forEach((c, j) => {
+              data[c] = e[j];
+            });
+            return data;
+          })}
+          isLoading={false}
+          pagination={{
+            defaultPageSize: 10,
+            showSizeChanger: true,
+            pageSizeOptions: ['10', '20', '30', '40', '50'],
+          }}
+          rowKey={() => {
+            return uuidv4();
+          }}
+          scroll={{ x: 800, y: 250 }}
+        />
+      ) : (
+        adaptableComponent()
+      )}
+      <div className='predict-options-buttons'>
+        <Button
+          icon={<CopyOutlined />}
+          type='primary'
+          style={{ margin: '6px 6px 0px 0px' }}
+          onClick={() => utils.copyToClipboard()}
+        >
+          Copiar
+        </Button>
+        <a
+          href={utils.downloadFile(experimentInference)}
+          download='predict-file'
+        >
+          <Button
+            icon={<DownloadOutlined />}
+            type='primary'
+            style={{ margin: '6px 6px 0px 0px' }}
+          >
+            Fazer download
+          </Button>
+        </a>
+      </div>
+    </>
+  );
+
   return (
     <>
       {!experimentInference ? (
@@ -65,59 +124,7 @@ const InferenceTestResultModalContent = (props) => {
           retryTest={retryTest}
         />
       ) : (
-        <>
-          {'ndarray' in experimentInference ? (
-            <CommonTable
-              columns={experimentInference.names.map((name) => ({
-                title: name,
-                dataIndex: name,
-                key: name,
-                width: 100,
-              }))}
-              dataSource={experimentInference.ndarray.map((e, i) => {
-                const data = { key: i };
-                experimentInference.names.forEach((c, j) => {
-                  data[c] = e[j];
-                });
-                return data;
-              })}
-              isLoading={false}
-              pagination={{
-                defaultPageSize: 10,
-                showSizeChanger: true,
-                pageSizeOptions: ['10', '20', '30', '40', '50'],
-              }}
-              rowKey={() => {
-                return uuidv4();
-              }}
-              scroll={{ x: 800, y: 250 }}
-            />
-          ) : (
-            adaptableComponent()
-          )}
-          <div className='predict-options-buttons'>
-            <Button
-              icon={<CopyOutlined />}
-              type='primary'
-              style={{ margin: '6px 6px 0px 0px' }}
-              onClick={() => utils.copyToClipboard()}
-            >
-              Copiar
-            </Button>
-            <a
-              href={utils.downloadFile(experimentInference)}
-              download='predict-file'
-            >
-              <Button
-                icon={<DownloadOutlined />}
-                type='primary'
-                style={{ margin: '6px 6px 0px 0px' }}
-              >
-                Fazer download
-              </Button>
-            </a>
-          </div>
-        </>
+        renderModalBody()
       )}
     </>
   );
