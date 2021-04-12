f980fef33054b6671f47400f65aa8808f6dcb263
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-04-12 23:09:16 +0000

    Create models module

---
+++ src/Models/Projects.typeDef.js
@@ -0,0 +1,26 @@
+/* globals Experiments */
+// TODO: Criar Experiments type def (model)
+
+/**
+ * Projeto (Project).
+ *
+ * https://platiagro.github.io/projects/#model-Project
+ *
+ * @typedef {object} Project
+ *
+ * @property {string} uuid Project id
+ * @property {string} name Project name
+ * @property {string} description	Project description
+ * @property {Experiments} experiments Project experiments list
+ * @property {string} createdAt	Project creation date
+ * @property {string} updatedAt	Project last update date
+ * @property {string} hasExperiment	Project has experiment
+ * @property {string} hasDeployment	Project has deployment
+ * @property {string} hasPreDeployment Project has pre deployment
+ */
+
+/**
+ * Projetos (Projects).
+ *
+ * @typedef {Project[]} Projects
+ */
