d4e9007b5d06348899bfabaa6586bc3dded2e13d
Schaffer <mr.giancarloschaffer@gmail.com>
2021-04-16 11:51:42 -0300

    Change container to hooks pattern

--- src/containers/PropertiesResizableContainer/PropertiesResizableContainer.jsx
+++ src/containers/PropertiesResizableContainer/PropertiesResizableContainer.jsx
@@ -1,33 +1,17 @@
-// REACT LIBS
-import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-// COMPONENTS
 import { PropertiesPanel } from 'components';
 
-// STATES
-const mapStateToProps = (state) => {
-  return {
-    operatorName: state.operatorReducer.name,
-  };
+const selectedOperatorNameSelector = ({ operatorReducer }) => {
+  return operatorReducer.name;
 };
 
-const PropertiesResizableContainer = (props) => {
-  const { operatorName } = props;
+const PropertiesResizableContainer = () => {
+  const operatorName = useSelector(selectedOperatorNameSelector);
 
   return <PropertiesPanel title={operatorName} />;
 };
 
-// PROP TYPES
-PropertiesResizableContainer.propTypes = {
-  operatorName: PropTypes.string,
-};
-
-// DEFAULT PROPS
-PropertiesResizableContainer.defaultProps = {
-  operatorName: undefined,
-};
-
 // EXPORT DEFAULT
-export default connect(mapStateToProps, null)(PropertiesResizableContainer);
+export default PropertiesResizableContainer;
