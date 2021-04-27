962a9806f05ad3727a64a5a16c73881ffff5dfce
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-04-27 13:36:11 +0000

    Rectify reducer bug

--- src/store/projects/index.js
+++ src/store/projects/index.js
@@ -4,4 +4,4 @@ import * as Actions from './Projects.actions';
 import * as ActionTypes from './Projects.actionTypes';
 
 export { Reducer, Actions, ActionTypes, Selectors };
-export default Reducer.default;
+export default Reducer;
