9a5311b75257a83480c9fcf3b2e29e5d5d6a1523
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-04-27 00:11:38 +0000

    Rectify action types

--- src/store/projects/Projects.actions.js
+++ src/store/projects/Projects.actions.js
@@ -6,7 +6,7 @@
 import { message } from 'antd';
 
 // ACTION TYPES
-import actionTypes from './Projects.actionTypes';
+import * as actionTypes from './Projects.actionTypes';
 
 // SERVICES
 import projectsApi from 'services/ProjectsApi';
