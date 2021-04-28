78b42011d3c68b40ddc20183d172a2217bb67bb1
Luan Eduardo da Costa <luan1346@gmail.com>
2021-04-28 16:38:25 -0300

    Define routes declaratively

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { Switch, Route } from 'react-router-dom';
 
-import Routes from 'routes';
+import { MainRoutes } from 'routes';
 import MainSider from 'components/MainSider/_';
 
 import './style.less';
@@ -12,17 +11,7 @@ const App = () => (
     <MainSider />
 
     <Layout>
-      <Switch>
-        {Routes.map((route) => {
-          const { exact, path, strict } = route;
-
-          return (
-            <Route key={path} exact={exact} path={path} strict={strict}>
-              <route.component />
-            </Route>
-          );
-        })}
-      </Switch>
+      <MainRoutes />
     </Layout>
   </Layout>
 );
