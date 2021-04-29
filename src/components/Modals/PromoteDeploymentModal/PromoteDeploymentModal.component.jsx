298bd13eb9dec93054d0f7058149dd36529ec9dd
Schaffer Jr <30741185+schafferjrdev@users.noreply.github.com>
2021-04-29 18:53:17 -0300

    Feature/use promote deployment modal (#403)
    * Change routerProps to history for push
    
    * control input disable and value
    
    * use promote deployment modal
    
    * Add disabled to run deployment button
    
    * Fix smells on handleRunDeployment parameters
    
    * Change operatorsSelector to use deployment
    
    * Adjust prefix, sufix and modal width
    
    * Change boolean name and import component

--- src/components/Modals/PromoteDeploymentModal/PromoteDeploymentModal.component.jsx
+++ src/components/Modals/PromoteDeploymentModal/PromoteDeploymentModal.component.jsx
@@ -1,29 +1,39 @@
-// CORE LIBS
-import PropTypes from "prop-types";
-import React, { useState } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-
-// UI LIBS
-import { Modal, Button, Input } from "antd";
-import { CopyOutlined } from "@ant-design/icons";
-import "./style.less";
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Modal, Button, Input } from 'antd';
+import { CopyToClipboard } from 'components';
+import { CopyOutlined } from '@ant-design/icons';
+import './style.less';
 
 const PromoteDeploymentModal = (props) => {
-  const { visible, onClose, onConfirm, urlPrefix, urlSuffix } = props;
+  const {
+    visible,
+    onClose,
+    onConfirm,
+    urlPrefix,
+    urlSuffix,
+    isInputDisabled,
+    loading,
+    initialInputValue,
+  } = props;
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState(initialInputValue);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
+  const handleConfirm = () => {
+    onConfirm(inputValue);
+  };
+
   const selectBefore = (
-    <div className="modal-text-color" disabled>
+    <div className='modal-text-color' disabled>
       {urlPrefix}
     </div>
   );
   const selectAfter = (
-    <div className="modal-text-color" disabled>
+    <div className='modal-text-color' disabled>
       {urlSuffix}
     </div>
   );
@@ -33,22 +43,29 @@ const PromoteDeploymentModal = (props) => {
       visible={visible}
       title={<strong>Implantar fluxo</strong>}
       onCancel={onClose}
-      onOk={onConfirm}
-      okText="Implantar"
-      cancelText="Cancelar"
-      width="600px"
+      onOk={handleConfirm}
+      okText='Implantar'
+      cancelText='Cancelar'
+      width='875px'
       destroyOnClose
+      loading={loading}
     >
       <p>URL de implantação</p>
-      <div className="modal-components-configuration">
+      <div className='modal-components-configuration'>
         <Input
-          id="implantation-input"
+          id='implantation-input'
           addonBefore={selectBefore}
           addonAfter={selectAfter}
           onChange={handleInputChange}
+          value={inputValue}
+          disabled={isInputDisabled}
         ></Input>
         <CopyToClipboard text={`${urlPrefix}${inputValue}${urlSuffix}`}>
-          <Button className="ant-btn-primary-inverse" type="default" icon={<CopyOutlined />}>
+          <Button
+            className='ant-btn-primary-inverse'
+            type='default'
+            icon={<CopyOutlined />}
+          >
             Copiar
           </Button>
         </CopyToClipboard>
@@ -57,7 +74,6 @@ const PromoteDeploymentModal = (props) => {
   );
 };
 
-// PROP TYPES
 PromoteDeploymentModal.propTypes = {
   /** modal visible */
   visible: PropTypes.bool.isRequired,
@@ -67,6 +83,20 @@ PromoteDeploymentModal.propTypes = {
   onClose: PropTypes.func.isRequired,
   /** modal confirm handler */
   onConfirm: PropTypes.func.isRequired,
+  /** boolean to disable input */
+  isInputDisabled: PropTypes.bool,
+  /** Prefix for url input */
+  urlPrefix: PropTypes.string.isRequired,
+  /** Suffix for url input */
+  urlSuffix: PropTypes.string.isRequired,
+  /** Initial value on input */
+  initialInputValue: PropTypes.string,
+};
+
+PromoteDeploymentModal.defaultProps = {
+  loading: false,
+  isInputDisabled: false,
+  initialInputValue: 'initial value',
 };
 
 export default PromoteDeploymentModal;
