e95267d1a4e57d1cf91cc7f15e3046231740faf0
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-05-04 00:26:56 +0000

    Refactor experiments reducer

--- src/store/projects/experiments/experimentsReducer.js
+++ src/store/projects/experiments/experimentsReducer.js
@@ -1,88 +1,29 @@
-// ACTION TYPES
+// correção de bug do eslint/jsdoc
+/* eslint-disable-next-line */
+/* global ProjectsStore */
 import * as EXPERIMENTS_TYPES from './experiments.actionTypes';
-import experimentRunsActionTypes from './experimentRuns/actionTypes';
-
-// INITIAL STATE
-const initialState = [];
 
 /**
- * experiments reducer
+ * Projects experiments reducer
  *
- * @param state
- * @param action
- * @returns {Array} Experiments list
+ * @param {ProjectsStore} state State object
+ * @param {object} action Action object
+ * @param {string} action.type Action type
+ * @param {object} action.payload Action payload
+ * @returns {ProjectsStore} New state
  */
-const experimentsReducer = (state = initialState, action = undefined) => {
+const experimentsReducer = (state, action) => {
   switch (action.type) {
-    //reset initial fetch of experiments
-    case EXPERIMENTS_TYPES.CLEAR_ALL_EXPERIMENTS:
-      return [];
-
-    // SUCCESS
-    // fetch experiments success
     case EXPERIMENTS_TYPES.FETCH_EXPERIMENTS_SUCCESS:
-      return [...action.experiments];
-    // create experiment success
     case EXPERIMENTS_TYPES.CREATE_EXPERIMENT_SUCCESS:
-      return [...state, action.experiment];
-    // edit experiment name success
     case EXPERIMENTS_TYPES.UPDATE_EXPERIMENT_SUCCESS:
-      return [...action.experiments];
-    // delete experiment success
     case EXPERIMENTS_TYPES.DELETE_EXPERIMENT_SUCCESS:
-      return [...action.experiments];
-    // organize experiments success
     case EXPERIMENTS_TYPES.ORGANIZE_EXPERIMENTS_SUCCESS:
-      return [...action.experiments];
+      return { ...state, projects: action.payload.projects };
 
-    // // // // // // //
-
-    // FAIL
-    // fetch experiments fail
-    case EXPERIMENTS_TYPES.FETCH_EXPERIMENTS_FAIL:
-      return [...state];
-    // create experiment fail
-    case EXPERIMENTS_TYPES.CREATE_EXPERIMENT_FAIL:
-      return [...state];
-    // update experiment fail
-    case EXPERIMENTS_TYPES.UPDATE_EXPERIMENT_FAIL:
-      return [...state];
-    // delete experiment fail
-    case EXPERIMENTS_TYPES.DELETE_EXPERIMENT_FAIL:
-      return [...state];
-    // organize experiments fail
-    case EXPERIMENTS_TYPES.ORGANIZE_EXPERIMENTS_FAIL:
-      return [...state];
-
-    // // // // // // //
-
-    // EXPERIMENT RUNS
-    case experimentRunsActionTypes.EXPERIMENT_RUN_SUCCEEDED:
-      return [...action.experiments];
-    case experimentRunsActionTypes.EXPERIMENT_RUN_NOT_SUCCEEDED:
-      return [...action.experiments];
-
-    // DEFAULT
     default:
       return state;
   }
 };
 
-// SELECTOR
-
-/**
- * Select experiment by id
- *
- * @param {object} state state
- * @param {string} experimentId Experiment UUID
- * @returns {object} The specified experiment, or an empty object
- */
-export const getExperimentById = (state, experimentId) => {
-  const experiment = state.experimentsReducer.find(
-    (exp) => exp.uuid === experimentId
-  );
-  return experiment ? experiment : {};
-};
-
-// EXPORT
 export default experimentsReducer;
