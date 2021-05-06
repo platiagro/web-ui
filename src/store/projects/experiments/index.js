5b79e8d26bf0c2cabf57e14f2054c8ee77fb4f88
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-05-06 17:56:55 +0000

    Remove unused experiments reducer file

--- src/store/projects/experiments/index.js
+++ src/store/projects/experiments/index.js
@@ -1,8 +1,5 @@
 import * as Actions from './experiments.actions';
 import * as EXPERIMENTS_TYPES from './experiments.actionTypes';
 import * as Selectors from './experiments.selectors';
-import Reducer from './experiments.reducer';
-
-export default Reducer;
 
 export { Actions, EXPERIMENTS_TYPES, Selectors };
