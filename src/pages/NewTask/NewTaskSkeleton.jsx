c9ad42fa4e477011da6d4a991c59486167b26101
Luan Eduardo da Costa <luan1346@gmail.com>
2021-05-27 14:28:33 -0300

    Fix NewTask page loading skeleton

--- src/pages/NewTask/NewTaskSkeleton.jsx
+++ src/pages/NewTask/NewTaskSkeleton.jsx
@@ -4,9 +4,35 @@ import { Skeleton } from 'antd';
 const NewTaskSkeletons = () => {
   return (
     <div className='new-task-page-content-panels-left-skeletons'>
-      <Skeleton.Button size='large' active />
-      <Skeleton.Button size='large' active />
-      <Skeleton.Button size='large' active />
+      <Skeleton.Button
+        className='new-task-page-content-panels-left-skeleton'
+        size='large'
+        active
+      />
+
+      <Skeleton.Button
+        className='new-task-page-content-panels-left-skeleton'
+        size='large'
+        active
+      />
+
+      <Skeleton.Button
+        className='new-task-page-content-panels-left-skeleton'
+        size='large'
+        active
+      />
+
+      <Skeleton.Button
+        className='new-task-page-content-panels-left-skeleton'
+        size='large'
+        active
+      />
+
+      <Skeleton.Button
+        className='new-task-page-content-panels-left-skeleton'
+        size='large'
+        active
+      />
     </div>
   );
 };
