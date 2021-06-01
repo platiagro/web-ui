63ae1a43d779be8ef0baa9642fe1a803261911d3
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-01 14:53:28 -0300

    Fix DraggableTabs component JsDoc, remove comments and organize code

--- src/components/TabsBar/DraggableTabs/index.js
+++ src/components/TabsBar/DraggableTabs/index.js
@@ -1,41 +1,26 @@
-// CORE LIBS
 import React from 'react';
-import PropTypes from 'prop-types';
-
-// UI LIBS
 import { Tabs } from 'antd';
+import PropTypes from 'prop-types';
 import { DragSource, DropTarget } from 'react-dnd';
 
-// DRAG AND DROP PROPERTIES
-// tab type
 const TAB_TYPE = 'TAB';
-// tab target
 const tabTargetConfig = {
-  // drop event
   drop(props, monitor) {
-    // monitor constant
     const mon = monitor;
-    // get dragging tab key
     const dragKey = mon.getItem().index;
-    // get hovering tab key
     const hoverKey = props.index;
 
-    // dragging tab is same of hovering tab
     if (dragKey === hoverKey) {
       return;
     }
 
-    // moving tab
     props.moveTabNode(dragKey, hoverKey);
-    // setting new key to dragging tab
     mon.getItem().index = hoverKey;
   },
 };
-// tab source
+
 const tabSourceConfig = {
-  // drag event
   beginDrag(props) {
-    // getting tab properties
     return {
       id: props.id,
       index: props.index,
@@ -43,13 +28,10 @@ const tabSourceConfig = {
   },
 };
 
-// DRAG AND DROP TAB NODES CONTAINERS
-// tab node
 const TabNode = ({ connectDragSource, connectDropTarget, children }) => {
-  // connecting drag and drop
   return connectDragSource(connectDropTarget(children));
 };
-// wrapped tab node (configuring drag and drop)
+
 const WrappedTabNode = DropTarget(TAB_TYPE, tabTargetConfig, (connect) => ({
   connectDropTarget: connect.dropTarget(),
 }))(
@@ -59,25 +41,16 @@ const WrappedTabNode = DropTarget(TAB_TYPE, tabTargetConfig, (connect) => ({
   }))(TabNode)
 );
 
-/**
- * Draggable Tabs.
- * This component is responsible for displaying and configuring draggable tabs.
- */
 const DraggableTabs = (props) => {
   const { activeTab, children, onMoveTab } = props;
 
-  // tabs array
   const tabs = [];
   React.Children.forEach(children, (c) => {
     tabs.push(c);
   });
 
-  // COMPONENTS RENDERS
-  // tab bar
   const renderTabBar = (propsTabBar, DefaultTabBar) => (
-    // tab bar
     <DefaultTabBar {...propsTabBar}>
-      {/* wrapped tabs */}
       {(node) => (
         <WrappedTabNode key={node.key} index={node.key} moveTabNode={onMoveTab}>
           {node}
@@ -86,9 +59,7 @@ const DraggableTabs = (props) => {
     </DefaultTabBar>
   );
 
-  // RENDER
   return (
-    // drag and drop provider
     <Tabs
       activeKey={activeTab}
       type='card'
@@ -101,24 +72,17 @@ const DraggableTabs = (props) => {
   );
 };
 
-// PROP TYPES
 DraggableTabs.propTypes = {
-  /** draggable tabs active tab key */
   activeTab: PropTypes.string,
-  /** draggable tabs or tab */
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
-  /** draggable tabs move tab handler */
   onMoveTab: PropTypes.func.isRequired,
 };
 
-// PROP DEFAULT VALUES
 DraggableTabs.defaultProps = {
-  /** draggable tabs active tab key */
   activeTab: undefined,
 };
 
-// EXPORT
 export default DraggableTabs;
