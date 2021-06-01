1741b020b62a955e26c8f52e9774557488461e7d
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-01 14:52:49 -0300

    Fix ErrorRow component JsDoc, remove comments and organize code

--- src/components/ErrorRow/ErrorRow.component.jsx
+++ src/components/ErrorRow/ErrorRow.component.jsx
@@ -1,42 +1,28 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
+import { BugOutlined } from '@ant-design/icons';
 
-import { BugOutlined } from "@ant-design/icons";
-
-import "./style.less";
-
-/**
- * An error row.
- *
- * @param {object} props Component props
- *
- * @returns {ErrorRow} Component
- *
- * @component
- */
+import './style.less';
 
 const ErrorRow = (props) => {
   const { title, dateTime, message } = props;
 
   return (
-    <div className="ErrorRow-Body">
-      <div className="ErrorRow-Header">
-        <BugOutlined className="ErrorRow-Icon" />
+    <div className='ErrorRow-Body'>
+      <div className='ErrorRow-Header'>
+        <BugOutlined className='ErrorRow-Icon' />
         {title}
         {dateTime}
       </div>
-      <div className="ErrorRow-Content">{message}</div>
+
+      <div className='ErrorRow-Content'>{message}</div>
     </div>
   );
 };
 
-// PROP TYPES
 ErrorRow.propTypes = {
-  /** */
   title: PropTypes.string.isRequired,
-  /** */
   dateTime: PropTypes.string.isRequired,
-  /** */
   message: PropTypes.string.isRequired,
 };
 
