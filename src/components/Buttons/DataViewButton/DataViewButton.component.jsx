cc8cfff1f1e51f8b9a025deee4b2f7690c3d1984
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-01 14:51:14 -0300

    Fix DataViewButton component JsDoc, remove comments and organize code

--- src/components/Buttons/DataViewButton/DataViewButton.component.jsx
+++ src/components/Buttons/DataViewButton/DataViewButton.component.jsx
@@ -1,76 +1,28 @@
-// REACT LIBS
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// ANTD ICON
 import { TableOutlined } from '@ant-design/icons';
 
-// UI COMPONENTS
 import { Button } from 'uiComponents';
 
-/**
- * Data view primary rounded button.
- *
- * @component
- *
- * @param {object} props Component props
- *
- * @returns {DataViewButton} Component
- *
- * @example
- * // Data View Button click handler
- * const handleClick = () => alert('Click!');
- * // Data View Button is loading
- * const isLoading = false;
- * // Data View Button is disabled
- * const isDisabled = false;
- *
- * // rendering component
- * return (
- *   <DataViewButton
- *     isDisabled={isDisabled}
- *     isLoading={isLoading}
- *     handleClick={handleClick}
- *   />
- * );
- */
-const DataViewButton = (props) => {
-  // destructuring props
-  const { isDisabled, isLoading, handleClick } = props;
-
-  // button shape
-  const icon = <TableOutlined />;
-
-  // button icon
-  const shape = 'round';
-
-  // button type
-  const type = 'primary';
-
-  // rendering component
+const DataViewButton = ({ isDisabled, isLoading, handleClick }) => {
   return (
     <Button
-      icon={icon}
+      icon={<TableOutlined />}
       isDisabled={isDisabled}
       isLoading={isLoading}
       handleClick={handleClick}
-      shape={shape}
-      type={type}
+      shape='round'
+      type='primary'
     >
       Visualizar dados
     </Button>
   );
 };
 
-// PROP TYPES
 DataViewButton.propTypes = {
-  /** Data View Button click handler */
   handleClick: PropTypes.func.isRequired,
-  /** Data View Button is loading */
   isLoading: PropTypes.bool.isRequired,
-  /** Data View Button is disabled */
   isDisabled: PropTypes.bool.isRequired,
 };
 
-// EXPORT DEFAULT
 export default DataViewButton;
