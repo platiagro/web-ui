77e16c2bd834211ac24b23f1ec4024cc5f3106bb
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-01 14:53:58 -0300

    Fix DatasetDrawer component JsDoc, remove comments and organize code

--- src/pages/Experiments/Experiment/Drawer/DatasetDrawer/index.js
+++ src/pages/Experiments/Experiment/Drawer/DatasetDrawer/index.js
@@ -1,60 +1,32 @@
-// CORE LIBS
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-// CONTAINERS
 import {
-  DatasetUploadInputBlockContainer,
   DataViewButtonContainer,
+  DatasetUploadInputBlockContainer,
 } from 'containers';
-
-// COMPONENTS
 import { PropertyBlock } from 'components';
 
-/**
- * Dataset Drawer.
- * This component is responsible for displaying dataset content in drawer.
- *
- * @param props
- */
-const DatasetDrawer = (props) => {
-  // PROPS / CONSTANTS
-  // destructuring props
-  const { columns } = props;
+const DatasetDrawer = ({ columns }) => {
+  const showDataViewButton = useMemo(() => {
+    return !!columns && columns.length > 0;
+  }, [columns]);
 
-  // show data view button
-  const showDataViewButton = columns !== undefined && columns.length > 0;
-
-  // RENDERS
-  // render data view button
-  const renderDataViewButton = () => {
-    // rendering table
-    return columns.length === 0 ? null : (
-      // input container
-      <PropertyBlock>
-        <DataViewButtonContainer />
-      </PropertyBlock>
-    );
-  };
-
-  // render component
   return (
-    // div container
     <div>
-      {/* dataset upload input block container */}
       <DatasetUploadInputBlockContainer />
 
-      {/* data view button */}
-      {showDataViewButton && renderDataViewButton()}
+      {showDataViewButton && (
+        <PropertyBlock>
+          <DataViewButtonContainer />
+        </PropertyBlock>
+      )}
     </div>
   );
 };
 
-// PROP TYPES
 DatasetDrawer.propTypes = {
-  /** dataset drawer dataset columns */
   columns: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-// EXPORT
 export default DatasetDrawer;
