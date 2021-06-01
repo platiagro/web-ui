e695c9e8a010d3a0e1a0dfd40c64939bfc1f6078
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-01 14:52:23 -0300

    Fix DebugRow component JsDoc, remove comments and organize code

--- src/components/DebugRow/DebugRow.component.jsx
+++ src/components/DebugRow/DebugRow.component.jsx
@@ -1,44 +1,27 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
+import { CodeOutlined } from '@ant-design/icons';
 
-import { CodeOutlined } from "@ant-design/icons";
+import './style.less';
 
-import "./style.less";
-
-/**
- * A debug row.
- *
- * @param {object} props Component props
- *
- * @returns {DebugRow} Component
- *
- * @component
- */
-
-const DebugRow = (props) => {
-  const { title, dateTime, message } = props;
-
+const DebugRow = ({ title, dateTime, message }) => {
   return (
-    <div className="DebugRow-Body">
-      <div className="DebugRow-Header">
-        <CodeOutlined className="DebugRow-Icon" />
+    <div className='DebugRow-Body'>
+      <div className='DebugRow-Header'>
+        <CodeOutlined className='DebugRow-Icon' />
         {title}
         {dateTime}
       </div>
-      <div className="DebugRow-Content">{message}</div>
+
+      <div className='DebugRow-Content'>{message}</div>
     </div>
   );
 };
 
-// PROP TYPES
 DebugRow.propTypes = {
-  /** */
   title: PropTypes.string.isRequired,
-  /** */
   dateTime: PropTypes.string.isRequired,
-  /** */
   message: PropTypes.string.isRequired,
 };
-
 
 export default DebugRow;
