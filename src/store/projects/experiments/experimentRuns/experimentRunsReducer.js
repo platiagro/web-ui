b0729f2aad8942e50b79167d7d4b6d8784816037
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-02 09:39:37 -0300

    Fix experiment runs reducer JsDoc, remove comments and organize code

--- src/store/projects/experiments/experimentRuns/experimentRunsReducer.js
+++ src/store/projects/experiments/experimentRuns/experimentRunsReducer.js
@@ -1,40 +1,30 @@
-// ACTION TYPES
 import actionTypes from './actionTypes';
 
-// INITIAL STATE
 const initialState = [];
 
-/**
- * experiment runs reducer
- *
- * @param state
- * @param action
- */
 const experimentRunsReducer = (state = initialState, action = undefined) => {
   switch (action.type) {
-    // SUCCESS
-    // experiment runs
     case actionTypes.FETCH_EXPERIMENT_RUNS_SUCCESS:
       return [...action.runs];
+
     case actionTypes.CREATE_EXPERIMENT_RUN_SUCCESS:
-      return [...state, {runId: action.runId}];
+      return [...state, { runId: action.runId }];
+
     case actionTypes.DELETE_EXPERIMENT_RUN_SUCCESS:
       return [...action.runs];
-    
-    // FAIL
-    // experiment runs
+
     case actionTypes.FETCH_EXPERIMENT_RUNS_FAIL:
       return [...state];
+
     case actionTypes.CREATE_EXPERIMENT_RUN_FAIL:
       return [...state];
+
     case actionTypes.DELETE_EXPERIMENT_RUN_FAIL:
       return [...state];
 
-    // DEFAULT
     default:
       return state;
   }
 };
 
-// EXPORT
 export default experimentRunsReducer;
