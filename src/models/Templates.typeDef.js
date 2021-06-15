e4508bd2499b22ae117dc286db47959d9137bcf2
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-06-15 16:41:23 +0000

    Create templates typeDef

---
+++ src/models/Templates.typeDef.js
@@ -0,0 +1,21 @@
+// TODO: Criar Tasks type def (model)
+
+/**
+ * Template.
+ *
+ * https://platiagro.github.io/projects/#model-Templates
+ *
+ * @typedef {object} Template
+ *
+ * @property {string} uuid Template id
+ * @property {string} name Template name
+ * @property {Tasks} tasks Template tasks
+ * @property {string} createdAt	Project creation date
+ * @property {string} updatedAt	Project last update date
+ */
+
+/**
+ * Templates.
+ *
+ * @typedef {Template[]} Templates
+ */
