e2902ace67c97df712902293ad75227fcf7d34fd
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-06-27 17:32:13 +0000

    Adjust create template service

--- src/services/TemplatesApi.js
+++ src/services/TemplatesApi.js
@@ -1,3 +1,9 @@
+// correção de bug do eslint/jsdoc
+/* eslint-disable-next-line */
+/* global TemplateCreatable */
+
+/* API REFERENCE: https://platiagro.github.io/projects/#/Templates */
+
 import axios from 'axios';
 
 const URL = process.env.REACT_APP_PROJECTS_API || 'http://localhost:8080';
@@ -20,16 +26,11 @@ const listTemplates = () => {
 /**
  * Create Template
  *
- * @param {string} templateName Template name
- * @param {string} experimentId Experiment UUID
+ * @param {TemplateCreatable} template Template creatable object
  * @returns {Promise} Request Promise
  */
-const createTemplate = (templateName, experimentId) => {
-  const body = {
-    name: templateName,
-    experimentId,
-  };
-  return templatesApi.post(templatesPath, body);
+const createTemplate = (template) => {
+  return templatesApi.post(templatesPath, template);
 };
 
 /**
