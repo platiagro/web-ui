33bfd6688913b7158188d952056704a5c3421829
Cedric Matheus <cedric.ms.alves@gmail.com>
2021-07-28 21:33:42 +0000

    Rectify rerender bug

--- src/pages/Experiments/Experiment/Drawer/GenericDrawer/GenericDrawerContainer.jsx
+++ src/pages/Experiments/Experiment/Drawer/GenericDrawer/GenericDrawerContainer.jsx
@@ -3,7 +3,7 @@ import { Empty } from 'antd';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { useIsLoading } from 'hooks';
+import { useIsLoading, useDeepEqualSelector } from 'hooks';
 import { OPERATORS_TYPES } from 'store/operators';
 import { ParameterGroup, PropertyBlock } from 'components';
 import { updateExperimentOperatorParameterRequest } from 'store/operator';
@@ -24,15 +24,31 @@ const parametersLatestTrainingSelector = ({ operatorReducer }) => {
   return operatorReducer.parametersLatestTraining;
 };
 
-const GenericDrawerContainer = () => {
+// para adicionar o loading em cada parametro, precisamos
+// renderizar cada parametro como se fosse um container
+//
+// desativamos o eslint para não ficar exigindo proptypes para esse container
+/* eslint-disable */
+const OperatorParameter = ({ parameterData }) => {
   const { projectId, experimentId } = useParams();
+
   const dispatch = useDispatch();
 
-  const operator = useSelector(operatorSelector);
-  const parameters = useSelector(parametersSelector);
   const trainingLoading = useSelector(trainingLoadingSelector);
   const parametersLatestTraining = useSelector(
     parametersLatestTrainingSelector
+  );
+  const operator = useDeepEqualSelector(operatorSelector);
+
+  const loadingString = `${operator.uuid}-${parameterData.name}`;
+
+  const valueLatestTraining = parametersLatestTraining
+    ? parametersLatestTraining[parameterData.name]
+    : parameterData.value;
+
+  const parameterLoading = useIsLoading(
+    OPERATORS_TYPES.CLEAR_OPERATORS_FEATURE_PARAMETERS_REQUEST,
+    loadingString
   );
 
   const setOperatorParameterHandler = (parameterName, parameterValue) => {
@@ -47,34 +63,20 @@ const GenericDrawerContainer = () => {
     );
   };
 
-  // para adicionar o loading em cada parametro, precisamos
-  // renderizar cada parametro como se fosse um container
-  //
-  // desativamos o eslint para não ficar exigindo proptypes para esse container
-  /* eslint-disable */
-  const OperatorParameter = ({ parameterData }) => {
-    const loadingString = `${operator.uuid}-${parameterData.name}`;
+  return (
+    <ParameterGroup
+      parameter={parameterData}
+      loading={parameterLoading}
+      trainingLoading={trainingLoading}
+      onChange={setOperatorParameterHandler}
+      valueLatestTraining={valueLatestTraining}
+    />
+  );
+};
+/* eslint-enable */
 
-    const parameterLoading = useIsLoading(
-      OPERATORS_TYPES.CLEAR_OPERATORS_FEATURE_PARAMETERS_REQUEST,
-      loadingString
-    );
-
-    const valueLatestTraining = parametersLatestTraining
-      ? parametersLatestTraining[parameterData.name]
-      : parameterData.value;
-
-    return (
-      <ParameterGroup
-        parameter={parameterData}
-        loading={parameterLoading}
-        trainingLoading={trainingLoading}
-        onChange={setOperatorParameterHandler}
-        valueLatestTraining={valueLatestTraining}
-      />
-    );
-  };
-  /* eslint-enable */
+const GenericDrawerContainer = () => {
+  const parameters = useDeepEqualSelector(parametersSelector);
 
   if (!parameters?.length) {
     return (
