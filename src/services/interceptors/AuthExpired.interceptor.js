e660a16b3621dbfe4d2194d326ddbc0ee3b2dd03
Luan Eduardo da Costa <luan1346@gmail.com>
2021-08-17 16:19:00 -0300

    Redirect to location that came in the error response data

--- src/services/interceptors/AuthExpired.interceptor.js
+++ src/services/interceptors/AuthExpired.interceptor.js
@@ -1,10 +1,11 @@
 export const response = {
   onRejected(error) {
-    // The HyperText Transfer Protocol (HTTP) 302 Found redirect status response code indicates that the resource requested has been temporarily moved to the URL given by the Location header. Learn More At https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
-    if (error?.response?.status === 302) {
+    const newLocation = error?.response?.data?.location;
+
+    if (newLocation) {
       // Redirect to the base route. This will show the login page to the user
       // window.location.assign saves the new route in the browser history
-      window.location.assign('/');
+      window.location.assign(newLocation);
       return;
     }
 
--- src/services/interceptors/AuthExpired.interceptor.test.js
+++ src/services/interceptors/AuthExpired.interceptor.test.js
@@ -12,15 +12,15 @@ describe('AuthExpired interceptor', () => {
     window.location = location;
   });
 
-  it('should change window.location when response status is 302', () => {
+  it('should change window.location when the error location exists', () => {
     const error = new Error('The request failed with error 302');
-    error.response = { status: 302 };
+    error.response = { status: 302, data: { location: '/dex/auth' } };
     const returnData = AuthExpiredInterceptor.response.onRejected(error);
-    expect(window.location.assign).toBeCalledWith('/');
+    expect(window.location.assign).toBeCalledWith(error.response.data.location);
     expect(returnData).toBe(undefined);
   });
 
-  it('should return rejected promise if response status is not 302', async () => {
+  it('should return rejected promise if error location is not defined', async () => {
     const error = new Error('The request failed with error 404');
     error.response = { status: 404 };
     const rejectedPromise = AuthExpiredInterceptor.response.onRejected(error);
@@ -28,19 +28,24 @@ describe('AuthExpired interceptor', () => {
     await expect(rejectedPromise).rejects.toEqual(error);
   });
 
-  it('should return rejected promise if the expression "error?.response?.status" returns undefined', async () => {
+  it('should return rejected promise if "error?.response?.data?.location" is undefined', async () => {
     const error1 = undefined;
     const rejectedPromise1 = AuthExpiredInterceptor.response.onRejected(error1);
     await expect(rejectedPromise1).rejects.toEqual(error1);
 
-    const error2 = new Error('An error without response');
+    const error2 = new Error('Without response');
     const rejectedPromise2 = AuthExpiredInterceptor.response.onRejected(error2);
     await expect(rejectedPromise2).rejects.toEqual(error2);
 
-    const error3 = new Error('An error with response but without status');
+    const error3 = new Error('With response but without data');
     error3.response = {};
     const rejectedPromise3 = AuthExpiredInterceptor.response.onRejected(error3);
     await expect(rejectedPromise3).rejects.toEqual(error3);
+
+    const error4 = new Error('With response and data but without location');
+    error4.response = { data: {} };
+    const rejectedPromise4 = AuthExpiredInterceptor.response.onRejected(error4);
+    await expect(rejectedPromise4).rejects.toEqual(error4);
 
     expect(window.location.assign).not.toBeCalled();
   });
