13d2b38ebd683fb0a6b6dc32aa9dcaea85be1ea0
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-03 14:03:07 -0300

    Create the marketplace store with types, reducer and selectors

---
+++ src/store/marketplace/index.js
@@ -0,0 +1,6 @@
+import * as MARKETPLACE_TYPES from './marketplace.actionTypes';
+export { MARKETPLACE_TYPES };
+
+export * from './marketplace.actions';
+export * from './marketplace.reducer';
+export * from './marketplace.selectors';
---
+++ src/store/marketplace/marketplace.reducer.js
@@ -0,0 +1,41 @@
+import * as MARKETPLACE_TASKS from './marketplace.actionTypes';
+
+export const initialState = {
+  totalMarketplaceTasks: 0,
+  marketplaceTasks: [],
+};
+
+export const marketplaceReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case MARKETPLACE_TASKS.FETCH_QUANTITY_SUCCESS: {
+      return {
+        ...state,
+        totalMarketplaceTasks: action.payload.totalMarketplaceTasks,
+      };
+    }
+
+    case MARKETPLACE_TASKS.FETCH_TASKS_SUCCESS: {
+      return {
+        ...state,
+        marketplaceTasks: action.payload.marketplaceTasks,
+      };
+    }
+
+    case MARKETPLACE_TASKS.FETCH_QUANTITY_FAIL: {
+      return {
+        ...state,
+        totalMarketplaceTasks: 0,
+      };
+    }
+
+    case MARKETPLACE_TASKS.FETCH_TASKS_FAIL: {
+      return {
+        ...state,
+        marketplaceTasks: [],
+      };
+    }
+
+    default:
+      return state;
+  }
+};
