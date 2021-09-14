698e071f6b80231c0ae619f087ab8228e239ec9b
Luan Eduardo da Costa <luan1346@gmail.com>
2021-09-13 21:05:12 -0300

    Use debouce in the marketplace initial page

--- src/pages/Marketplace/MarketplaceSearch.jsx
+++ src/pages/Marketplace/MarketplaceSearch.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router';
+import { SearchOutlined, LoadingOutlined } from '@ant-design/icons';
 
+import { useBooleanState, useDebounce } from 'hooks';
 import { MarketplaceColorfulComponent } from 'assets';
 
 const MarketplaceSearch = ({ numberOfTasks }) => {
+  const history = useHistory();
+
+  const [isSearching, handleStartSearching, handleStopSearching] =
+    useBooleanState();
+
   const handleSeeAllTasks = () => {
-    console.log('See all');
+    history.push('/marketplace/tarefas');
   };
+
+  const handleChangeSearchText = useDebounce({
+    delay: 1000,
+    startCallback() {
+      handleStartSearching();
+    },
+    stopCallback(search) {
+      handleStopSearching();
+      history.push({
+        pathname: '/marketplace/tarefas',
+        search: `?${new URLSearchParams({ search })}`,
+      });
+    },
+  });
 
   return (
     <div className={'marketplace-search'}>
@@ -20,8 +41,9 @@ const MarketplaceSearch = ({ numberOfTasks }) => {
 
           <Input
             className='marketplace-search-input'
+            onChange={(e) => handleChangeSearchText(e.target.value)}
             placeholder='Comece a digitar para buscar no marketplace'
-            suffix={<SearchOutlined />}
+            suffix={isSearching ? <LoadingOutlined /> : <SearchOutlined />}
           />
 
           <div className='marketplace-search-more'>
