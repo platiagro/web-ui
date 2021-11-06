871fb80f139844be3c50417611417252a8cec52d
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2021-11-06 08:50:38 -0300

    Renames prediction.Reducer.js to lowercase 'prediction.reducer.js'

--- src/store/prediction/index.js
+++ src/store/prediction/index.js
@@ -2,5 +2,5 @@ import * as PREDICTION_TYPES from './prediction.actionTypes';
 
 export { PREDICTION_TYPES };
 export * from './prediction.actions';
-export * from './prediction.Reducer';
+export * from './prediction.reducer';
 export * from './prediction.selectors';
